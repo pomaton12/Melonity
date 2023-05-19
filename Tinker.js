b928c9fee611b55402e0d143b5e617d6bc6f7cf5
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-19 07:24:12 -0500

    Update Tinker.js

--- Tinker.js
+++ Tinker.js
@@ -46,21 +46,7 @@ eval(`
 					if (remainingTime <= 20) {
 						let gale_force = localHero.GetAbilityByIndex(3);
 						if (gale_force && gale_force.IsExist() && gale_force.CanCast()) {
-							let target = ability.GetCursorTarget();
-							let targetPos = target.GetAbsOrigin();
-							let myPos = localHero.GetAbsOrigin();
-							let direction = (myPos - targetPos).Normalized();
-							let distance = (myPos - targetPos).Length2D();
-							if (distance <= 1000) {
-								gale_force.CastPosition(targetPos);
-							} else {
-								var angle = Math.acos(direction.Dot(localHero.GetForwardVector())) * 180 / Math.PI;
-								if (angle <= 90) {
-									gale_force.CastPosition(myPos + direction * 1000);
-								} else {
-									gale_force.CastPosition(myPos - direction * 1000);
-								}
-							}
+							gale_force.CastNoTarget();
 						}
 					}
 				}
