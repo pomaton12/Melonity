8ba5124deb894ffb84193af36452a3c48b93eb90
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-19 09:22:30 -0500

    Update Tinker.js

--- Tinker.js
+++ Tinker.js
@@ -49,7 +49,7 @@ eval(`
 							let enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY).length;
 							let targetPos = enemies;
 							let myPos = localHero.GetAbsOrigin();
-							let direction = targetPos.clone().sub(myPos).normalize();
+							let direction = targetPos.sub(myPos).normalize();
 							let distance = (myPos - targetPos).Length2D();
 							if (distance <= 1000) {
 								gale_force.CastPosition(targetPos);
