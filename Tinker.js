bf28f282c03374947cee84ef60e1178f75ed1354
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-19 12:11:21 -0500

    Update Tinker.js

--- Tinker.js
+++ Tinker.js
@@ -32,7 +32,7 @@ eval(`
 		})
 		.SetNameLocale('ru', 'штормовая сила')
 		.GetValue();
-	Menu.GetFolder(['Heroes', 'Intelligence', 'Windranger', 'Gale Force Use']).SetImage('panorama/images/items/stormcrafter_png.vtex_c');
+	Menu.GetFolder(path_, 'Gale Force Use').SetImage('panorama/images/items/stormcrafter_png.vtex_c');
 
 	// Definición de la función OnUpdate
 	AutoSaverWindrunner.OnUpdate = () => {
