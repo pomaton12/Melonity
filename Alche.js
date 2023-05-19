c4277bb1513c2801dde5d4c189f3d5ab4cf41fc8
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-19 01:20:02 -0500

    Update Alche.js

--- Alche.js
+++ Alche.js
@@ -15,7 +15,70 @@
   \**********************************/
 /***/ (() => {
 
-eval("const AutoSaverAlchemist = {}; let localHero; const path_ = ['Heroes', 'Strength', 'Alchemist', 'Evader']; let isUiEnabled = Menu.AddToggle(path_, 'Evader conconcoction', true).OnChange(state => { isUiEnabled = state.newValue; }).SetNameLocale('ru', 'Автододж бутылки').GetValue(); let itemSelection = Menu.AddMultiSelect(path_, 'Item selection', ['panorama/images/items/black_king_bar_png.vtex_c', 'panorama/images/items/manta_png.vtex_c'], [true, true]).OnChange((state) => { itemSelection = state.newValue; }).SetNameLocale('ru', 'Выбор предмета').GetValue(); AutoSaverAlchemist.OnUpdate = () => { if (localHero && isUiEnabled) { if (localHero.GetUnitName() !== "npc_dota_hero_alchemist") return; const modifiers = localHero.GetModifiers(); for (let modifier of modifiers) { if (modifier.GetName() === 'modifier_alchemist_unstable_concoction') { const remainingTime = modifier.GetRemainingTime(); if (remainingTime <= -6.45) { const manta = localHero.GetItem('item_manta', true); const bkb = localHero.GetItem('item_black_king_bar', true); if (itemSelection[1] && manta && manta.GetCooldown() === 0) { manta.CastNoTarget(); } else{ if (itemSelection[0] && bkb && bkb.GetCooldown() === 0 && manta.GetCooldown() 0) { bkb.CastNoTarget(); } } } } } } }; AutoSaverAlchemist.OnScriptLoad = AutoSaverAlchemist.OnGameStart = () => { localHero = EntitySystem.GetLocalHero(); }; AutoSaverAlchemist.OnGameEnd = () => { localHero = null; }; RegisterScript(AutoSaverAlchemist);");
+eval(`
+  // Definición del objeto AutoSaverAlchemist
+  const AutoSaverAlchemist = {};
+
+  // Declaración de la variable localHero
+  let localHero;
+
+  // Definición del array path_
+  const path_ = ['Heroes', 'Strength', 'Alchemist', 'Evader'];
+
+  // Creación del toggle isUiEnabled
+  let isUiEnabled = Menu.AddToggle(path_, 'Evader conconcoction', true)
+      .OnChange(state => {
+          isUiEnabled = state.newValue;
+      })
+      .SetNameLocale('ru', 'Автододж бутылки')
+      .GetValue();
+
+  // Creación del multiselect itemSelection
+  let itemSelection = Menu.AddMultiSelect(path_, 'Item selection', ['panorama/images/items/black_king_bar_png.vtex_c', 'panorama/images/items/manta_png.vtex_c'], [true, true])
+      .OnChange((state) => {
+          itemSelection = state.newValue;
+      })
+      .SetNameLocale('ru', 'Выбор предмета')
+      .GetValue();
+
+  // Definición de la función OnUpdate
+  AutoSaverAlchemist.OnUpdate = () => {
+      if (localHero isUiEnabled) {
+          if (localHero.GetUnitName() !== "npc_dota_hero_alchemist")
+              return;
+          const modifiers = localHero.GetModifiers();
+          for (let modifier of modifiers) {
+              if (modifier.GetName() === 'modifier_alchemist_unstable_concoction') {
+                  const remainingTime = modifier.GetRemainingTime();
+                  if (remainingTime <= -6.45) {
+                      const manta = localHero.GetItem('item_manta', true);
+                      const bkb = localHero.GetItem('item_black_king_bar', true);
+                      if (itemSelection[1] && manta && manta.GetCooldown() === 0) {
+                          manta.CastNoTarget();
+                          return;
+                      } else if (itemSelection[0] && bkb && bkb.GetCooldown() === 0) {
+                          bkb.CastNoTarget();
+                          return;
+                      }
+                  }
+              }
+          }
+      }
+  };
+
+  // Definición de la función OnScriptLoad
+  AutoSaverAlchemist.OnScriptLoad = AutoSaverAlchemist.OnGameStart = () => {
+      localHero = EntitySystem.GetLocalHero();
+  };
+
+  // Definición de la función OnGameEnd
+  AutoSaverAlchemist.OnGameEnd = () => {
+      localHero = null;
+  };
+
+  // Registro del script
+  RegisterScript(AutoSaverAlchemist);
+`);
 
 /***/ })
 
