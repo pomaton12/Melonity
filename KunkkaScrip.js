baec4e9c7a0b17a7254a13a040bae75dd9c444da
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-20 16:03:07 -0500

    Update KunkkaScrip.js

--- KunkkaScrip.js
+++ KunkkaScrip.js
@@ -31,46 +31,33 @@ eval(`
 	let bkbEnemies = {};
 	//===============================
 	UseShardKunkka.OnUpdate = () => {
-  if (localHero && isUiEnabled1) {
-    if (localHero.GetUnitName() !== "npc_dota_hero_kunkka") {
-      return;
+if (localHero && isUiEnabled1) {
+  if (localHero.GetUnitName() !== "npc_dota_hero_kunkka") {
+    return;
+  }
+
+  // Obtener habilidad "Torrent Storm"
+  let torrentStorm = localHero.GetAbilityByIndex(3);
+
+  // Verificar si la habilidad está lista para ser lanzada
+  if (torrentStorm && torrentStorm.IsExist() && torrentStorm.CanCast()) {
+
+// Verificar si algún héroe enemigo ha sido afectado por el ultimate
+    let enemies = localHero.FindEnemiesInRange(torrentStorm.GetCastRange());
+    let enemyAffected = false;
+    for (let i = 0; i < enemies.length; i++) {
+      if (enemies[i].IsHero() && enemies[i].IsAlive() && enemies[i].IsEnemy(localHero)) {
+        enemyAffected = true;
+        break;
+      }
     }
 
-    // Obtener habilidad "Torrent Storm"
-    let torrentStorm = localHero.GetAbilityByIndex(3);
-
-    // Verificar si la habilidad está lista para ser lanzada
-    if (torrentStorm && torrentStorm.IsExist() && torrentStorm.CanCast()) {
-      // Buscar enemigos cercanos
-      let enemies = EntitySystem.GetHeroesList().filter(hero => {
-        return hero.IsAlive()  && hero.IsVisible() && !hero.IsIllusion();
-      });
-
-      // Seleccionar el punto de lanzamiento de "Torrent Storm"
-      let targetPoint;
-      if (enemies.length > 0) {
-        if (enemies.length === 1) {
-          targetPoint = enemies[0].GetPosition();
-        } else {
-          let center = { x: 0, y: 0, z: 0 };
-          for (let i = 0; i < enemies.length; i++) {
-            center.x += enemies[i].GetPosition().x;
-            center.y += enemies[i].GetPosition().y;
-            center.z += enemies[i].GetPosition().z;
-          }
-          center.x /= enemies.length;
-          center.y /= enemies.length;
-          center.z /= enemies.length;
-          targetPoint = center;
-        }
-      } else {
-        return;
-      }
-
-      // Lanzar "Torrent Storm" en el punto seleccionado
-      torrentStorm.CastPosition(targetPoint);
+    // Si se encuentra al menos un héroe enemigo afectado por el ultimate, lanzar "Torrent Storm" en el punto seleccionado
+    if (enemyAffected) {
+      torrentStorm.CastPosition(localHero.GetAbsOrigin());
     }
   }
+}
 		
 	  	if (localHero && isUiEnabled2) {
 
