19c7781360aa649a8a5f50a823f9920bf81cf6c7
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-20 04:48:07 -0500

    Update Tinker.js

--- Tinker.js
+++ Tinker.js
@@ -18,66 +18,59 @@
 eval(`
 	// Definición del objeto AutoSaverAlchemist
 	const AutoSaverWindrunner = {};
-
-	// Declaración de la variable localHero
 	let localHero;
 
-	// Definición del array path_
 	const path_ = ['Heroes', 'Intelligence', 'Windranger'];
-
-	// Creación del toggle isUiEnabled
 	let isUiEnabled = Menu.AddToggle(path_, 'Gale Force Use', true);
 	isUiEnabled.SetImage('panorama/images/spellicons/windrunner_gale_force_png.vtex_c');
 
-	// Definición de la función OnUpdate
 	let previousEnemyPositions = {};
-	//===============================
+
 	AutoSaverWindrunner.OnUpdate = () => {
-	  if (localHero && isUiEnabled) {
-	    if (localHero.GetUnitName() !== "npc_dota_hero_windrunner")
-	      return;
-	    const modifiers = localHero.GetModifiers();
-	    for (let modifier of modifiers) {
-	      if (modifier.GetName() === 'modifier_windrunner_focusfire') {
-		const remainingTime = modifier.GetRemainingTime();
-		if (remainingTime <= 20) {
-		  let gale_force = localHero.GetAbilityByIndex(3);
-		  if (gale_force && gale_force.IsExist() && gale_force.CanCast()) {
-		    let enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
-		    for (let enemy of enemies) {
-		      let enemyId = enemy.GetPlayerID();
-		      let isAttacking = enemy.IsAttacking() && enemy.GetAttackTarget() === localHero;
-		      let currentPosition = enemy.GetAbsOrigin();
-		      let isEscaping = false;
+	  if (!localHero || !isUiEnabled.GetValue()) return;
 
-		      if (previousEnemyPositions[enemyId]) {
-			let previousPosition = previousEnemyPositions[enemyId];
-			let distanceMoved = currentPosition.sub(previousPosition).Length2D();
-			isEscaping = distanceMoved > 0 && !isAttacking;
+	  if (localHero.GetUnitName() !== "npc_dota_hero_windrunner") return;
+
+	  const modifiers = localHero.GetModifiers();
+	  for (let modifier of modifiers) {
+	    if (modifier.GetName() === 'modifier_windrunner_focusfire') {
+	      const remainingTime = modifier.GetRemainingTime();
+	      if (remainingTime <= 20) {
+		let gale_force = localHero.GetAbilityByIndex(3);
+		if (gale_force && gale_force.IsExist() && gale_force.CanCast()) {
+		  let enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
+		  for (let enemy of enemies) {
+		    let enemyId = enemy.GetPlayerID();
+		    let isAttacking = enemy.IsAttacking() && enemy.GetAttackTarget() === localHero;
+		    let currentPosition = enemy.GetAbsOrigin();
+		    let isEscaping = false;
+
+		    if (previousEnemyPositions[enemyId]) {
+		      let previousPosition = previousEnemyPositions[enemyId];
+		      let distanceMoved = currentPosition.sub(previousPosition).Length2D();
+		      isEscaping = distanceMoved > 0 && !isAttacking;
+		    }
+
+		    previousEnemyPositions[enemyId] = currentPosition;
+
+		    let vec1 = localHero.GetAbsOrigin();
+		    let vec2 = enemy.GetAbsOrigin();
+		    let distance = vec1.sub(vec2).Length2D();
+
+		    if (distance <= 1000) {
+		      if (enemy.HasModifier('modifier_windrunner_shackleshot') || enemy.HasModifier('modifier_sheepstick_debuff') || enemy.HasModifier('modifier_black_king_bar_immune')) {
+			continue;
 		      }
 
-		      previousEnemyPositions[enemyId] = currentPosition;
-
-		      let vec1 = localHero.GetAbsOrigin();
-		      let vec2 = enemy.GetAbsOrigin();
-		      let distance = vec1.sub(vec2).Length2D();
-
-		      if (distance <= 1000) {
-			// Comprobar si el enemigo está aturdido con Shackleshot, Scythe of Vyse o tiene la BKB activada
-			if (enemy.HasModifier('modifier_windrunner_shackleshot') || enemy.HasModifier('modifier_sheepstick_debuff') || enemy.HasModifier('modifier_black_king_bar_immune')) {
-			  continue;
-			}
-
-			let pushDirection;
-			if (isAttacking) {
-			  pushDirection = vec2.sub(vec1).Normalized();
-			} else if (isEscaping) {
-			  pushDirection = vec1.sub(vec2).Normalized();
-			} else {
-			  continue;
-			}
-			gale_force.CastPosition(vec1.add(pushDirection));
+		      let pushDirection;
+		      if (isAttacking) {
+			pushDirection = vec2.sub(vec1).Normalized();
+		      } else if (isEscaping) {
+			pushDirection = vec1.sub(vec2).Normalized();
+		      } else {
+			continue;
 		      }
+		      gale_force.CastPosition(localHero.GetAbsOrigin().add(pushDirection.mul(300)));
 		    }
 		  }
 		}
@@ -86,17 +79,14 @@ eval(`
 	  }
 	};
 
-	// Definición de la función OnScriptLoad
 	AutoSaverWindrunner.OnScriptLoad = AutoSaverWindrunner.OnGameStart = () => {
-		localHero = EntitySystem.GetLocalHero();
+	  localHero = EntitySystem.GetLocalHero();
 	};
 
-	// Definición de la función OnGameEnd
 	AutoSaverWindrunner.OnGameEnd = () => {
-		localHero = null;
+	  localHero = null;
 	};
 
-	// Registro del script
 	RegisterScript(AutoSaverWindrunner);
 
 `);
