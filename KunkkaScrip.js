8ff6d054ec84bccad9dff4dfbda89c234b3dd624
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-20 15:02:56 -0500

    Update KunkkaScrip.js

--- KunkkaScrip.js
+++ KunkkaScrip.js
@@ -31,46 +31,46 @@ eval(`
 	let bkbEnemies = {};
 	//===============================
 	UseShardKunkka.OnUpdate = () => {
-  		if (localHero && isUiEnabled1) {
-		    if (localHero.GetUnitName() !== "npc_dota_hero_kunkka") {
-		      return;
-		    }
+  if (localHero && isUiEnabled1) {
+    if (localHero.GetUnitName() !== "npc_dota_hero_kunkka") {
+      return;
+    }
 
-		    // Obtener la habilidad "Torrent Storm"
-		    let torrentStorm = localHero.GetAbilityByIndex(3);
+    // Obtener habilidad "Torrent Storm"
+    let torrentStorm = localHero.GetAbilityByIndex(3);
 
-		    // Verificar si la habilidad está lista para ser lanzada
-		    if (torrentStorm.IsFullyCastable() && (torrentStorm.GetCooldownTimeRemaining() === 0)) {
-		      // Buscar enemigos cercanos
-		      let enemies = EntitySystem.GetHeroesList().filter(hero => {
-			return hero.IsAlive() && hero.IsEnemy(localHero) && hero.IsVisible() && !hero.IsIllusion();
-		      });
+    // Verificar si la habilidad está lista para ser lanzada
+    if (torrentStorm.CanCast() && (torrentStorm.GetCooldownTimeRemaining() === 0)) {
+      // Buscar enemigos cercanos
+      let enemies = EntitySystem.GetHeroesList().filter(hero => {
+        return hero.IsAlive() && hero.IsEnemy(localHero) && hero.IsVisible() && !hero.IsIllusion();
+      });
 
-		      // Seleccionar el punto de lanzamiento de "Torrent Storm"
-		      let targetPoint;
-		      if (enemies.length > 0) {
-			if (enemies.length === 1) {
-			  targetPoint = enemies[0].GetPosition();
-			} else {
-			  let center = { x: 0, y: 0, z: 0 };
-			  for (let i = 0; i < enemies.length; i++) {
-			    center.x += enemies[i].GetPosition().x;
-			    center.y += enemies[i].GetPosition().y;
-			    center.z += enemies[i].GetPosition().z;
-			  }
-			  center.x /= enemies.length;
-			  center.y /= enemies.length;
-			  center.z /= enemies.length;
-			  targetPoint = center;
-			}
-		      } else {
-			return;
-		      }
+      // Seleccionar el punto de lanzamiento de "Torrent Storm"
+      let targetPoint;
+      if (enemies.length > 0) {
+        if (enemies.length === 1) {
+          targetPoint = enemies[0].GetPosition();
+        } else {
+          let center = { x: 0, y: 0, z: 0 };
+          for (let i = 0; i < enemies.length; i++) {
+            center.x += enemies[i].GetPosition().x;
+            center.y += enemies[i].GetPosition().y;
+            center.z += enemies[i].GetPosition().z;
+          }
+          center.x /= enemies.length;
+          center.y /= enemies.length;
+          center.z /= enemies.length;
+          targetPoint = center;
+        }
+      } else {
+        return;
+      }
 
-		      // Lanzar "Torrent Storm" en el punto seleccionado
-		      torrentStorm.CastPosition(targetPoint);
-		    }
-		  }
+      // Lanzar "Torrent Storm" en el punto seleccionado
+      torrentStorm.CastPosition(targetPoint);
+    }
+  }
 		
 	  	if (localHero && isUiEnabled2) {
 
