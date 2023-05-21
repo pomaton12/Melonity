8543d44fe5cb36ada524a5cc1858a35611d60d70
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-20 23:50:13 -0500

    Update KunkkaScrip.js

--- KunkkaScrip.js
+++ KunkkaScrip.js
@@ -15,70 +15,8 @@
   \******************************/
 /***/ (() => {
 
-//eval(`
-  
-  const AutoStealBara = {};
-  let local;
-  let localPlayer;
-  let chargedStrikeAbility;
-  let enemyHeroes;
-  let phylacteryItem;
-  let damageFromPhylactery = 150;
-  const path_ = ['Heroes', 'Strength', 'Spirit Breaker'];
-  let isUiEnabled = Menu.AddToggle(path_, 'AutoSteal Phylactery', true)
-      .OnChange(state => {
-          isUiEnabled = state.newValue;
-      })
-      .SetNameLocale('ru', 'АвтоСтил Phylactery')
-      .GetValue();
+eval("const AutoStealBara = {};\r\nlet localHero;\r\nlet localPlayer;\r\nlet chargedStrikeAbility;\r\nlet enemyHeroes;\r\nlet phylacteryItem;\r\nlet damageFromPhylactery = 150;\r\nconst path_ = ['Heroes', 'Strength', 'Spirit Breaker'];\r\nlet isUiEnabled = Menu.AddToggle(path_, 'AutoSteal Phylactery', true)\r\n    .OnChange(state => {\r\n    isUiEnabled = state.newValue;\r\n})\r\n    .SetNameLocale('ru', 'АвтоСтил Phylactery')\r\n    .GetValue();\r\nfunction stop() {\r\n    localPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_STOP, null, null, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY, localHero, false, true);\r\n}\r\nAutoStealBara.OnUpdate = () => {\r\n    if (localHero && isUiEnabled) {\r\n        if (localHero.GetUnitName() !== \"npc_dota_hero_spirit_breaker\")\r\n            return;\r\n        if (!chargedStrikeAbility) {\r\n            chargedStrikeAbility = localHero.GetAbilityByIndex(0);\r\n        }\r\n        phylacteryItem = localHero.GetItem(\"item_phylactery\", true);\r\n        if (phylacteryItem) {\r\n            if (Engine.OnceAt(2)) {\r\n                enemyHeroes = EntitySystem.GetHeroesList();\r\n            }\r\n            for (let i = 0; i < enemyHeroes.length; i++) {\r\n                let hero = enemyHeroes[i];\r\n                if (hero) {\r\n                    if (hero.GetTeamNum() !== localHero.GetTeamNum() && hero.GetHealth() <= damageFromPhylactery && chargedStrikeAbility.CanCast() && phylacteryItem.CanCast() && hero.IsAlive()) {\r\n                        chargedStrikeAbility.CastTarget(hero);\r\n                        if (Engine.OnceAt(0.6)) {\r\n                            stop();\r\n                        }\r\n                    }\r\n                }\r\n            }\r\n        }\r\n    }\r\n};\r\nAutoStealBara.OnScriptLoad = AutoStealBara.OnGameStart = () => {\r\n    localPlayer = EntitySystem.GetLocalPlayer();\r\n    localHero = EntitySystem.GetLocalHero();\r\n};\r\nAutoStealBara.OnGameEnd = () => {\r\n    localPlayer = null;\r\n    localHero = null;\r\n    chargedStrikeAbility = null;\r\n    phylacteryItem = null;\r\n};\r\nRegisterScript(AutoStealBara);\r\n\n\n//# sourceURL=webpack:///./src/AutoStealBara.ts?");
 
-  function stop() {
-      localPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_STOP, null, null, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY, localHero, false, true);
-  }
-
-  AutoStealBara.OnUpdate = () => {
-      if (localHero && isUiEnabled) {
-          if (localHero.GetUnitName() !== "npc_dota_hero_spirit_breaker")
-              return;
-          if (!chargedStrikeAbility) {
-              chargedStrikeAbility = localHero.GetAbilityByIndex(0);
-          }
-          phylacteryItem = localHero.GetItem("item_phylactery", true);
-          if (phylacteryItem) {
-              if (Engine.OnceAt(2)) {
-                  enemyHeroes = EntitySystem.GetHeroesList();
-              }
-              for (let i = 0; i < enemyHeroes.length; i++) {
-                  let hero = enemyHeroes[i];
-                  if (hero) {
-                      if (hero.GetTeamNum() !== localHero.GetTeamNum() && hero.GetHealth() <= damageFromPhylactery && chargedStrikeAbility.CanCast() && phylacteryItem.CanCast() && hero.IsAlive()) {
-                          chargedStrikeAbility.CastTarget(hero);
-                          if (Engine.OnceAt(0.6)) {
-                              stop();
-                          }
-                      }
-                  }
-              }
-          }
-      }
-  };
-
-  AutoStealBara.OnScriptLoad = AutoStealBara.OnGameStart = () => {
-      localPlayer = EntitySystem.GetLocalPlayer();
-      localHero = EntitySystem.GetLocalHero();
-  };
-
-  AutoStealBara.OnGameEnd = () => {
-      localPlayer = null;
-      localHero = null;
-      chargedStrikeAbility = null;
-      phylacteryItem = null;
-  };
-
-  RegisterScript(AutoStealBara);
-  
- `);
- 
 /***/ })
 
 /******/ 	});
