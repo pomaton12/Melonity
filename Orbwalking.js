a3d3219e1f97af2531819b986e00afc79f2faffc
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-22 02:02:37 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -17,7 +17,7 @@ eval(`
 	// Definición del array path_
 	const path_ = ['Heroes', 'Orbwalking'];
 
-	// Creación del toggle isUiEnabled1  para habilidad 4
+	// Creación del toggle isUiEnabled1  para activar Orbwalking
 	let isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true);
 		
 	// Creación del target hero o posicion del mouse
@@ -27,18 +27,55 @@ eval(`
 	
 	Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
 	// Definición de la función OnUpdate
-
+	let lastAttackTime = 0;
+	
 	//===============================
 	HitRunHeros.OnUpdate = () => {
-		if (localHero && isUiEnabled1) {
-			if (localHero.GetUnitName() !== "npc_dota_hero_windrunner")
-			  return;
-		}
-	  
-	  	if (localHero && DisplayMode) {
+		  if (!localHero || !isUiEnabled1) {
+		    return;
+		  }
 
-		}
-	  
+		  const myPlayer = EntitySystem.GetLocalPlayer();
+		  const mousePos = myPlayer.GetCursorPosition();
+		  const attackRange = localHero.GetAttackRange();
+		  const attackSpeed = localHero.GetAttackSpeed();
+		  const attackPoint = localHero.GetAttackPoint();
+		  const enemies = EntitySystem.GetEntitiesByClass('C_DOTA_BaseNPC_Hero').filter(hero => hero.IsEnemy(localHero));
+
+		  let targetEnemy = null;
+
+		  if (DisplayMode === 0) { // To Enemy
+		    targetEnemy = enemies.reduce((closest, enemy) => {
+		      const distanceToEnemy = localHero.GetAbsOrigin().sub(enemy.GetAbsOrigin()).Length2D();
+		      const distanceToClosest = closest ? localHero.GetAbsOrigin().sub(closest.GetAbsOrigin()).Length2D() : Infinity;
+
+		      return distanceToEnemy < distanceToClosest ? enemy : closest;
+		    }, null);
+		  } else if (DisplayMode === 1) { // Mouse position
+		    targetEnemy = enemies.reduce((closest, enemy) => {
+		      const distanceToMouse = enemy.GetAbsOrigin().sub(mousePos).Length2D();
+		      const distanceToClosest = closest ? closest.GetAbsOrigin().sub(mousePos).Length2D() : Infinity;
+
+		      return distanceToMouse < distanceToClosest ? enemy : closest;
+		    }, null);
+		  }
+
+		  if (targetEnemy) {
+		    const distanceToTarget = localHero.GetAbsOrigin().sub(targetEnemy.GetAbsOrigin()).Length2D();
+		    const gameTime = Game.GetGameTime();
+		    const attackCooldown = 1 / attackSpeed;
+
+		    if (distanceToTarget <= attackRange) {
+		      if (localHero.CanAttack(targetEnemy) && gameTime - lastAttackTime > attackCooldown + attackPoint) {
+			myPlayer.AttackTarget(localHero, targetEnemy);
+			lastAttackTime = gameTime;
+		      } else {
+			myPlayer.Move(localHero, mousePos);
+		      }
+		    } else {
+		      myPlayer.Move(localHero, targetEnemy.GetAbsOrigin());
+		    }
+		  }
 	};
 	// Definición de la función OnScriptLoad
 	HitRunHeros.OnScriptLoad = HitRunHeros.OnGameStart = () => {
