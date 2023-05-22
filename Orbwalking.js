b27b66770973eb4498b158da7130f8a7f062442a
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-22 07:59:32 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -8,97 +8,81 @@
 /***/ (() => {
 
 eval(`
-	// Definición del objeto HitRunHeros
 	const HitRunHeros = {};
 
-	// Declaración de la variable localHero
 	let localHero;
 
-	// Definición del array path_
 	const path_ = ['Heroes', 'Orbwalking'];
 
-	// Creación del toggle isUiEnabled1  para activar Orbwalking
 	let isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true);
-	
-	// Asignacion de una tecla key bin
-	let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'KeyBind of OrbWalk', Enum.ButtonCode.KEY_NONE);
-	
-	// Creación del target hero o posicion del mouse
+
+	let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KEY_NONE);
+
 	let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'], 1)
-	.OnChange(state => DisplayMode = state.newValue)
-	.GetValue();
-	
+	  .OnChange(state => DisplayMode = state.newValue)
+	  .GetValue();
+
 	Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
-	// Definición de la función OnUpdate
+
 	let lastAttackTime = 0;
-	
-	//===============================
-HitRunHeros.OnUpdate = () => {
-  if (isUiEnabled1) {
-    //probar si el héroe local está atacando a un héroe enemigo
-    if (localHero.IsAttacking() && localHero.GetAttackTarget() != null) {
-      const enemy = localHero.GetAttackTarget();
-      const dist = enemy.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-      const attackRange = localHero.GetAttackRange();
-      // Si el héroe enemigo está fuera del rango de ataque, moverse hacia él
-      if (dist > attackRange) {
-        const dir = enemy.GetAbsOrigin().Subtract(localHero.GetAbsOrigin()).Normalized();
-        const pos = enemy.GetAbsOrigin().Add(dir.Multiply(100));
-        localHero.MoveTo(pos);
-      }
-      // Si el héroe enemigo está dentro del rango de ataque, atacarlo
-      else {
-        localHero.Attack(enemy);
-        // Si la opción "To Enemy" está seleccionada, moverse hacia el héroe enemigo
-        if (ToEnemy) {
-          const dir = enemy.GetAbsOrigin().Subtract(localHero.GetAbsOrigin()).Normalized();
-          const pos = enemy.GetAbsOrigin().Add(dir.Multiply(-100));
-          localHero.MoveTo(pos);
-        }
-        // Si la opción "Mouse position" está seleccionada, moverse hacia la posición del mouse
-        else if (MousePosition) {
-          const mousePos = Input.GetWorldCursorPos();
-          localHero.MoveTo(mousePos);
-        }
-      }
-    }
-    // Si la tecla KeyBindOrbwalk está presionada, buscar al héroe enemigo más cercano o el que está debajo del mouse
-    else if (KeyBindOrbwalk.IsKeyDown()) {
-      const mousePos = Input.GetWorldCursorPos();
-      const enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
-      enemies.sort((a, b) => {
-        const distA = a.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-        const distB = b.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-        return distA - distB;
-      });
-      let target = null;
-      for (const enemy of enemies) {
-        const dist = enemy.GetAbsOrigin().Distance(mousePos);
-        if (dist <= 100 || target == null) {
-          target = enemy;
-        }
-      }
-      // Atacar al héroe enemigo seleccionado
-      if (target != null) {
-        localHero.Attack(target);
-      }
-    }
-  } else {
-    // Orbwalking desactivado, no hacer nada
-  }
-};
-	// Definición de la función OnScriptLoad
+
+	HitRunHeros.OnUpdate = () => {
+	  if (isUiEnabled1) {
+	    if (localHero.IsAttacking() && localHero.GetAttackTarget() != null {
+	      const enemy = localHero.GetAttackTarget();
+	      const dist = enemy.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
+	      const attackRange = localHero.GetAttackRange();
+
+	      if (dist > attackRange) {
+		const dir = enemy.GetAbsOrigin().Subtract(localHero.GetAbsOrigin()).Normalized();
+		const pos = enemy.GetAbsOrigin().Add(dir.Multiply(100));
+		localHero.MoveTo(pos);
+	      } else {
+		localHero.Attack(enemy);
+
+		if (DisplayMode === 0) {
+		  const dir = enemy.GetAbsOrigin().Subtract(localHero.GetAbsOrigin()).Normalized();
+		  const pos = enemy.GetAbsOrigin().Add(dir.Multiply(-100));
+		  localHero.MoveTo(pos);
+		} else if (DisplayMode === 1) {
+		  const mousePos = Input.GetWorldCursorPos();
+		  localHero.MoveTo(mousePos);
+		}
+	      }
+	    } else if (KeyBindOrbwalk.IsKeyDown()) {
+	      const mousePos = Input.GetWorldCursorPos();
+	      const enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
+	      enemies.sort((a, b) => {
+		const distA = a.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
+		const distB = b.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
+		return distA - distB;
+	      });
+
+	      let target = null;
+	      for (const enemy of enemies) {
+		const dist = enemy.GetAbsOrigin().Distance(mousePos);
+		if (dist <= 100 || target == null) {
+		  target = enemy;
+		}
+	      }
+
+	      if (target != null) {
+		localHero.Attack(target);
+	      }
+	    }
+	  }
+	};
+
 	HitRunHeros.OnScriptLoad = HitRunHeros.OnGameStart = () => {
 	  localHero = EntitySystem.GetLocalHero();
 	};
 
-	// Definición de la función OnGameEnd
 	HitRunHeros.OnGameEnd = () => {
 	  localHero = null;
 	};
 
-	// Registro del script
 	RegisterScript(HitRunHeros);
+
 
 `);
 
