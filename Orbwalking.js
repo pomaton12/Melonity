6c955a825a85bf8433d4072499eb8c86a1fabba3
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-22 02:53:11 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -70,10 +70,10 @@ eval(`
         myPlayer.AttackTarget(localHero, targetEnemy);
         lastAttackTime = game_time;
       } else {
-        myPlayer.Move(localHero, mousePos);
+        myPlayer.MoveTo(mousePos.x, mousePos.y, mousePos.z);
       }
     } else {
-      myPlayer.Move(localHero, targetEnemy.GetAbsOrigin());
+      myPlayer.MoveTo(targetEnemy.GetAbsOrigin().x, targetEnemy.GetAbsOrigin().y, targetEnemy.GetAbsOrigin().z);
     }
   }
 	};
