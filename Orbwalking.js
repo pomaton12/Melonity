3bf89fe00b8b9b4b003288ade037b6dc9ee267fe
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-22 08:08:42 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -8,82 +8,80 @@
 /***/ (() => {
 
 eval(`
-	const HitRunHeros = {};
+constRunHeros = {};
 
-	let localHero;
+let localHero;
 
-	const path_ = ['Heroes', 'Orbwalking'];
+const path_ = ['Heroes', 'Orbwalking'];
 
-	let isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true);
+let isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true);
 
-	let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KEY_NONE);
+let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KEY_NONE);
 
-	let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'], 1)
-	  .OnChange(state => DisplayMode = state.newValue)
-	  .GetValue();
+let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'], 1)
+  .OnChange(state => DisplayMode = state.newValue)
+  .GetValue();
 
-	Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
+Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
 
-	let lastAttackTime = 0;
+let lastAttackTime = 0;
 
-	HitRunHeros.OnUpdate = () => {
-	  if (isUiEnabled1) {
-	    if (localHero.IsAttacking() && localHero.GetAttackTarget() != null ){
-	      const enemy = localHero.GetAttackTarget();
-	      const dist = enemy.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-	      const attackRange = localHero.GetAttackRange();
+HitRunHeros.OnUpdate = () => {
+  if (isUiEnabled1) {
+    (localHero.IsAttacking() && localHero.GetAttackTarget() != null) {
+      const enemy = localHero.GetAttackTarget();
+      const dist = enemy.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
+      const attackRange = localHero.GetAttackRange();
 
-	      if (dist > attackRange) {
-		const dir = enemy.GetAbsOrigin().Subtract(localHero.GetAbsOrigin()).Normalized();
-		const pos = enemy.GetAbsOrigin().Add(dir.Multiply(100));
-		localHero.MoveTo(pos);
-	      } else {
-		localHero.Attack(enemy);
+      if (dist > attackRange) {
+        const dir = enemy.GetAbsOrigin().Subtract(localHero.GetAbsOrigin()).Normalized();
+        const pos = enemy.GetAbsOrigin().Add(dir.Multiply(100));
+        localHero.MoveTo(pos);
+      } else {
+        localHero.Attack(enemy);
 
-		if (DisplayMode === 0) {
-		  const dir = enemy.GetAbsOrigin().Subtract(localHero.GetAbsOrigin()).Normalized();
-		  const pos = enemy.GetAbsOrigin().Add(dir.Multiply(-100));
-		  localHero.MoveTo(pos);
-		} else if (DisplayMode === 1) {
-		  const mousePos = Input.GetWorldCursorPos();
-		  localHero.MoveTo(mousePos);
-		}
-	      }
-	    } else if (KeyBindOrbwalk.IsKeyDown()) {
-	      const mousePos = Input.GetWorldCursorPos();
-	      const enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
-	      enemies.sort((a, b) => {
-		const distA = a.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-		const distB = b.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-		return distA - distB;
-	      });
+        if (DisplayMode === 0) {
+          const dir = enemy.GetAbsOrigin().Subtract(localHero.GetAbsOrigin()).Normalized();
+          const pos = enemy.GetAbsOrigin().Add(dir.Multiply(-100));
+          localHero.MoveTo(pos);
+        } else if (DisplayMode === 1) {
+          const mousePos = Input.GetWorldCursorPos();
+          localHero.MoveTo(mousePos);
+        }
+      }
+    } else if (KeyBindOrbwalk.IsKeyDown()) {
+      const mousePos = Input.GetWorldCursorPos();
+      const enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
+      enemies.sort((a, b) => {
+        const distA = a.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
+        const distB = b.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
+        return distA - distB;
+      });
 
-	      let target = null;
-	      for (const enemy of enemies) {
-		const dist = enemy.GetAbsOrigin().Distance(mousePos);
-		if (dist <= 100 || target == null) {
-		  target = enemy;
-		}
-	      }
+      let target = null;
+      for (const enemy of enemies) {
+        const dist = enemy.GetAbsOrigin().Distance(mousePos);
+        if (dist <= 100 || target == null) {
+          target = enemy;
+        }
+      }
 
-	      if (target != null) {
-		localHero.Attack(target);
-	      }
-	    }
-	  }
-	};
+      if (target != null) {
+        localHero.Attack(target);
+      }
+    }
+  }
+};
 
-	HitRunHeros.OnScriptLoad = HitRunHeros.OnGameStart = () => {
-	  localHero = EntitySystem.GetLocalHero();
-	};
+HitRunHeros.OnScriptLoad = HitRunHeros.OnGameStart = () => {
+  localHero = EntitySystem.GetLocalHero();
+};
 
-	HitRunHeros.OnGameEnd = () => {
-	  localHero = null;
-	};
+HitRunHeros.OnGameEnd = () => {
+  localHero = null;
+};
 
-	RegisterScript(HitRunHeros);
-
-
+RegisterScript(HitRunHeros);
 `);
 
 /***/ })
