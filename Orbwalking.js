5dfa7d61cf0acdeec0a19b0c51b57199c1f7db28
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-22 05:50:49 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -19,28 +19,34 @@ eval(`
 
 	// Creación del toggle isUiEnabled1  para activar Orbwalking
 	let isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true);
-	isUiEnabled1.SetImage('panorama/images/spellicons/kunkka_torrent_storm_png.vtex_c');
-		
+	
+	// Asignacion de una tecla key bin
+	let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'KeyBind of OrbWalk', Enum.ButtonCode.KEY_NONE);
+	
 	// Creación del target hero o posicion del mouse
-	let DisplayMode = Menu.AddComboBox(PATH, 'Display', ['To Enemy', 'Mouse position'], 1)
+	let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'], 1)
 	.OnChange(state => DisplayMode = state.newValue)
-	.GetValue()
-	.SetImage('panorama\images\hud\reborn\icon_speed_psd.vtex_c');
-
+	.GetValue();
+	
+	Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
 	// Definición de la función OnUpdate
-	let previousEnemyPositions = {};
-	let bkbEnemies = {};
+	let lastAttackTime = 0;
+	
 	//===============================
-	HitRunHeros.OnUpdate = () => {
-		if (localHero && isUiEnabled1) {
+HitRunHeros.OnUpdate = () => {
+  if (localHero || isUiEnabled1) {
 
-		}
-	  
-	  	if (localHero && DisplayMode) {
+  }
+  
+  if (localHero || KeyBindOrbwalk) {
 
-		}
-	  
-	};
+  }
+
+  if (localHero || DisplayMode) {
+
+  }
+
+};
 	// Definición de la función OnScriptLoad
 	HitRunHeros.OnScriptLoad = HitRunHeros.OnGameStart = () => {
 	  localHero = EntitySystem.GetLocalHero();
