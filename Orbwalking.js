ca1804989223005d058a278782a2f32341ea0043
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-22 14:51:05 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -28,8 +28,9 @@ let lastAttackTime = 0;
 
 HitRunHeros.OnUpdate = () => {
   if (localHero && isUiEnabled1.GetValue()) {
-  	console.log('Objetivo de ataque actual:', attackTarget);
+  	
 	const attackTarget = localHero.GetAttackTarget();
+	console.log('Objetivo de ataque actual:', attackTarget);
     const localHeroPosition = localHero.GetAbsOrigin();
     const enemy  = EntitySystem.GetHeroesList().filter(hero => hero.GetTeamNum() !== localHero.GetTeamNum() && hero.IsAlive() && localHeroPosition.Distance(hero.GetAbsOrigin()) <= 1000);
     const EnemyHero = enemy.reduce((closest, hero) => closest ? (localHeroPosition.Distance(hero.GetAbsOrigin()) < localHeroPosition.Distance(closest.GetAbsOrigin()) ? hero : closest) : hero, null);
