0bacc13c5bdbb46b7a9b5b87c16c2b83967d1df3
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-22 11:42:36 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -38,14 +38,14 @@ HitRunHeros.OnUpdate = () => {
       const attackRange = localHero.GetAttackRange();
 
       if (dist > attackRange) {
-        const dir = (enemyHeroPosition.sub(localHeroPosition)).Normalized();
+        const dir = (enemyHeroPosition.sub(localHeroPosition));
         const pos = EnemyHero.GetAbsOrigin().add(dir.mul(100));
         localHero.MoveTo(pos);
       } else {
         localHero.Attack(EnemyHero);
 
         if (DisplayMode === 0) {
-          const dir = (enemyHeroPosition.sub(localHeroPosition)).Normalized();
+          const dir = (enemyHeroPosition.sub(localHeroPosition));
           const pos = EnemyHero.GetAbsOrigin().add(dir.mul(-100));
           localHero.MoveTo(pos);
         } else if (DisplayMode === 1) {
