a22f3cd09d2711797d7a0dcab8d8aaa3e6895264
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-23 09:42:23 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -33,6 +33,7 @@ Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/ic
 function isHeroAttacking(hero, target) {
   // Comprueba si el héroe está dentro del rango de ataque del objetivo
   let distance = target.GetAbsOrigin().Distance(hero.GetAbsOrigin());
+  console.log('Objetivo de ataque actual:', distance);  
   if (distance <= hero.GetAttackRange()+5) {
     // Comprueba si el héroe está atacando actualmente
     if (hero.IsAttacking()) {
