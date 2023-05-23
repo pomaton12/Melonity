57cc22bb0d6fafa9ff24b72b609f50c8bc784551
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-23 14:42:34 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -8,147 +8,132 @@
 /***/ (() => {
 
 eval(`
-const HitRunHeros = {};
+local HitRunHeros = {}
 
-let localHero;
-let attackTarget;
-let EnemyHerotest;
+local localHero
+local attackTarget
+local EnemyHerotest
 
-const path_ = ['Heroes', 'Orbwalking'];
+local path_ = {'Heroes', 'Orbwalking'}
 
-let isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true);
+local isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true)
 
-let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KEY_NONE);
+local KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KEY_NONE)
 
-let isUiEnabled2 = Menu.AddToggle(path_, 'Kill Safe Pos', true);
+local isUiEnabled2 = Menu.AddToggle(path_, 'Kill Safe Pos', true)
 
-let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'], 1)
-  .OnChange(state => DisplayMode = state.newValue)
-  .GetValue();
-  
-let SafeDistanceUI = Menu.AddSlider(path_, 'Safe Distance (% Attack Range)', 1, 100, 100)
-	.OnChange(state => SafeDistanceUI = state.newValue)
-	.GetValue();
+local DisplayMode = Menu.AddComboBox(path_, 'Display', {'To Enemy', 'Mouse position'}, 1)
+  .OnChange(function(state) DisplayMode = state.newValue end)
+  .GetValue()
 
+local SafeDistanceUI = Menu.AddSlider(path_, 'Safe Distance (% Attack Range)', 1, 100, 100)
+	.OnChange(function(state) SafeDistanceUI = state.newValue end)
+	.GetValue()
 
+Menu.GetFolder({'Heroes', 'Orbwalking'}).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c')
 
-Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
+-- Función para evaluar attack target a un enemigo o amigo
+local function isHeroAttacking(hero, target)
+  -- Comprueba si el héroe está atacando actualmente
+  if hero.IsAttacking() then
+    return true
+  end
+  return false
+end
 
-//=========================================
-//       Evaluar Funciones
-//=========================================
-// 1 .- ====   Funcion para evaluar attack target a un enemigo o amigo
-function isHeroAttacking(hero, target) {
-    // Comprueba si el héroe está atacando actualmente
-    if (hero.IsAttacking()) {
-      return true;
-    }
-  return false;
-}
+-- Función para calcular distancia2D
+local function Dist2D(vec1, vec2)
+  if vec1 and vec2 then
+    local pos1 = (vec1.x and vec1 or (vec1.GetAbsOrigin and vec1.GetAbsOrigin() or 0    local pos2 = (vec2.x and vec2 or (vec2.GetAbsOrigin and vec2.GetAbsOrigin() or 0))
+    return pos1 and pos2 and pos1:sub(pos2):Length2D()
+  end
+  return -1
+end
 
-// 2 .- ====   Funcion para calcular distancia2D
-function Dist2D(vec1, vec2) {
-if (vec1 && vec2) {
-    let pos1 = (vec1.x ? (vec1) : (vec1.GetAbsOrigin ? (vec1.GetAbsOrigin()) : (0)));
-    let pos2 = (vec2.x ? (vec2) : (vec2.GetAbsOrigin ? (vec2.GetAbsOrigin()) : (0)));
-    return pos1 && pos2 && pos1.sub(pos2).Length2D();
-}
-    return -1;
-}
+-- Función POSICION DEL ANGULO
+local function IsntUndefined(value, withfalse)
+  return withfalse and value ~= false or value ~= nil and value ~= nil
+end
 
-// 3 .- ====   Funcion POSICION DEL ANGULO
-function IsntUndefined(value, withfalse) {
-	return withfalse ? (value !== false) : value !== undefined && value !== null;
-}
-function GetAngleToPos(_e1, _e2, prefer = _e2, inrad) {
-	let [a, b] = [IsntUndefined(_e1.x) ? _e1 : _e1.GetAbsOrigin(), IsntUndefined(_e2.x) ? _e2 : _e2.GetAbsOrigin()];
-	if (prefer == _e1) {
-	    [a, b] = [b, a];
-	}
-	let atan2 = Math.atan2(b.y - a.y, b.x - a.x);
-	return inrad ? atan2 : (atan2 * (180 / Math.PI));
-}
+local function GetAngleToPos(_e1, _e2, prefer, inrad)
+  local a, b = (IsntUndefined(_e1.x) and _e1 or (IsntUndefined(_e1.GetAbsOrigin) and _e1:GetAbsOrigin() or 0)), (IsntUndefined(_e2.x) and _e2 or (IsntUndefined(_e2.GetAbsOrigin) and _e2:GetAbsOrigin() or 0))
+  if prefer == _e1 then
+    a, b = b, a
+  end
+  local atan2 = math.atan2(b.y - a.y, b.x - a.x)
+  return inrad and atan2 or (atan2 * (180 / math.pi))
+end
 
-//=====================
-HitRunHeros.OnUpdate = () => {
-  if (localHero && isUiEnabled1.GetValue()) {
-	setTimeout(function() {
-	}, 600);
-	
-    const localHeroPosition = localHero.GetAbsOrigin();
-    const enemy  = EntitySystem.GetHeroesList().filter(hero => hero.GetTeamNum() !== localHero.GetTeamNum() && hero.IsAlive() && localHeroPosition.Distance(hero.GetAbsOrigin()) <= 1000);
-    const EnemyHero = enemy.reduce((closest, hero) => closest ? (localHeroPosition.Distance(hero.GetAbsOrigin()) < localHeroPosition.Distance(closest.GetAbsOrigin()) ? hero : closest) : hero, null);
-    const attackTarget = isHeroAttacking(localHero, EnemyHero);
+-- Función OnUpdate
+function HitRunHeros.OnUpdate()
+  if localHero and isUiEnabled1.GetValue() then
+    local localHeroPosition = localHero.GetAbsOrigin()
+    local enemy = EntitySystem.GetHeroesList():filter(function(hero) return hero.GetTeamNum() ~= localHero.GetTeamNum() and hero.IsAlive() and localHeroPosition:Distance(hero.GetAbsOrigin()) <= 1000 end)
+    local EnemyHero = enemy:reduce(function(closest, hero) return closest and (localHeroPosition:Distance(hero.GetAbsOrigin()) < localHeroPosition:Distance(closest.GetAbsOrigin()) and hero or closest) or hero end, nil)
+    local attackTarget = isHeroAttacking(localHero, EnemyHero)
 
+    if attackTarget then
+      local enemyHeroPosition = EnemyHero.GetAbsOrigin()
+      local dist = Dist2D(localHero.GetAbsOrigin(), EnemyHero.GetAbsOrigin())
+      local attackRange = localHero.GetAttackRange()
+      local newRange = attackRange * (SafeDistanceUI / 100)
+      localHero.SetAttackRange(newRange)
 
-	if (attackTarget) {
-	  	
-	  const enemyHeroPosition = EnemyHero.GetAbsOrigin();
-	  const dist = Dist2D(localHero.GetAbsOrigin(), EnemyHero.GetAbsOrigin());
-	  const attackRange = localHero.GetAttackRange();
-	  const newRange = attackRange * (SafeDistanceUI / 100);
-          localHero.SetAttackRange(newRange);
+      print('Rango de ataque actual:', localHero.SetAttackRange(newRange))
 
-	  console.log('Rango de ataque actual:', localHero.SetAttackRange(newRange));
-	  
-	  if (dist > attackRange) {
-	  
-	      if ( isUiEnabled2.GetValue()) {
-	         const pos = localHeroPosition.add(new Vector(100).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
-	         localHero.MoveTo(pos);
-	      }
-	  } else {
-	      //localHero.AttackTarget(EnemyHero);
-              if ( isUiEnabled2.GetValue()) {
-	         const pos = localHeroPosition.add(new Vector(-100).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
-	         localHero.MoveTo(pos);
-	      }
-	  }
-	  
-	  if (DisplayMode === 0) {
+      if dist > attackRange then
+        if isUiEnabled2.GetValue() then
+          local pos = localHeroPosition + Vector(100):Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition))
+          localHero.MoveTo(pos)
+        end
+      else
+        if isUiEnabled2.GetValue() then
+          local pos = localHeroPosition + Vector(-100):Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition))
+          localHero.MoveTo(pos)
+        end
+      end
 
-	  } else if (DisplayMode === 1) {
-	      const mousePos = Input.GetWorldCursorPos();
-	      localHero.MoveTo(mousePos);
-	      	setTimeout(function() {
-		}, 1000);
-	  }
-	}
+      if DisplayMode == 0 then
+      elseif DisplayMode == 1 then
+        local mousePos = Input.GetWorldCursorPos()
+        localHero.MoveTo(mousePos)
+        setTimeout(function() end, 1000)
+      end
+    end
 
-	
-    if (KeyBindOrbwalk.IsKeyDown()) {
-      const mousePos = Input.GetWorldCursorPos();
-      const enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
-      enemies.sort((a, b) => {
-        const distA = a.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-        const distB = b.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-        return distA - distB;
-      });
+    if KeyBindOrbwalk.IsKeyDown() then
+      local mousePos = Input.GetWorldCursorPos()
+      local enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY)
+      table.sort(enemies, function(a, b) return a.GetAbsOrigin():Distance(localHero.GetAbsOrigin()) < b.GetAbsOrigin():Distance(localHero.GetAbsOrigin()) end)
 
-      let target = null;
-      for (const enemy of enemies) {
-        const dist = enemy.GetAbsOrigin().Distance(mousePos);
-        if (dist <= 100 || target == null) {
-          target = enemy;
-        }
-      }
+      local target = nil
+      for _, enemy in ipairs(enemies) do
+        local dist = enemy.GetAbsOrigin():Distance(mousePos)
+        if dist <= 100 or target == nil then
+          target = enemy
+        end
+      end
 
-      if (target != null) {
-	localHero.AttackTarget(target);
-      }
-    }
-  }
-};
+      if target ~= nil then
+        localHero.AttackTarget(target)
+      end
+    end
+  end
+end
 
-HitRunHeros.OnScriptLoad = HitRunHeros.OnGameStart = () => {
-  localHero = EntitySystem.GetLocalHero();
-};
+-- Función OnScriptLoad y OnGameStart
+function HitRunHeros.OnScriptLoad()
+  localHero = EntitySystem.GetLocalHero()
+end
 
-HitRunHeros.OnGameEnd = () => {
-  localHero = null;
-};
+HitRunHeros.OnGameStart = HitRunHeros.OnScriptLoad
 
-RegisterScript(HitRunHeros);
+-- Función OnGameEnd
+function HitRunHeros.OnGameEnd()
+  localHero = nil
+end
+
+RegisterScript(HitRunHeros)
 `);
 
 /***/ })
