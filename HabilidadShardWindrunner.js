252d35e6b6ac5d4557312f25c004421f5d07a557
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-23 03:13:05 -0500

    Update HabilidadShardWindrunner.js

--- HabilidadShardWindrunner.js
+++ HabilidadShardWindrunner.js
@@ -29,10 +29,7 @@ eval(`
 	  if (localHero && isUiEnabled) {
 	    if (localHero.GetUnitName() !== "npc_dota_hero_windrunner")
 	      return;
-	      
-	    const attackTarget = localHero.GetAttackTarget();
-	    console.log('Objetivo de ataque actual:', attackTarget);	
-	    
+	      	    
 	    const modifiers = localHero.GetModifiers();
 	    for (let modifier of modifiers) {
 	      if (modifier.GetName() === 'modifier_windrunner_focusfire') {
@@ -60,6 +57,7 @@ eval(`
 		    for (let enemy of enemies) {
 		      let enemyId = enemy.GetPlayerID();
 		      let isAttacking = enemy.IsAttacking() && enemy.GetAttackTarget() === localHero;
+		      console.log('Objetivo de ataque actual:', enemy);	
 		      let currentPosition = enemy.GetAbsOrigin();
 		      let isEscaping = false;
 
