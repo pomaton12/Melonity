7d1869fa874a786919b1be7175bd9a04466ddf32
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-24 14:58:39 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -80,7 +80,7 @@ HitRunHeros.OnUpdate = () => {
     const EnemyHero = enemy.reduce((closest, hero) => closest ? (localHeroPosition.Distance(hero.GetAbsOrigin()) < localHeroPosition.Distance(closest.GetAbsOrigin()) ? hero : closest) : hero, null);
     const attackTarget = isHeroAttacking(localHero, EnemyHero);
     
-    const AttackTrgg = localHero.GetAttackTarget().CDOTA_BaseNPC_Hero;
+    const AttackTrgg = GetAttackTarget();
     console.log("El héroe local es.",AttackTrgg);
     
 	if (attackTarget) {
