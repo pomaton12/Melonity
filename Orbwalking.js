43cf6c5c6e2a28f74a634faf1c46158e1aca6c15
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-24 08:09:08 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -99,14 +99,14 @@ HitRunHeros.OnUpdate = () => {
 	  
 	      if ( isUiEnabled2.GetValue()) {
 	         const pos = localHeroPosition.add(new Vector(100).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
-		 setTimeout(function() {localHero.MoveTo(pos);}, 50); 
+		 setTimeout(function() {localHero.MoveTo(pos);}, 200); 
 	        		 
 	      }
 	  } else {
 	  
               if ( isUiEnabled2.GetValue() ) {
 	         const pos = localHeroPosition.add(new Vector(-100).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
-		 setTimeout(function() {localHero.MoveTo(pos); }, 50); 
+		 setTimeout(function() {localHero.MoveTo(pos); }, 200); 
 	         
 	      }
 	  }
