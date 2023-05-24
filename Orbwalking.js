b63348d87e40c30772f3a23c6723d7842bd159ab
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-24 01:27:56 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -24,12 +24,17 @@ let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KE
 let isUiEnabled2 = Menu.AddToggle(path_, 'Kill Safe Pos', true);
 
 let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'], 1)
-  .OnChange(state => DisplayMode = state.newValue)
-  .GetValue();
-  
-let SafeDistanceUI = Menu.AddSlider(path_, 'Safe Distance (% Attack Range)', 1, 100, 100)
+  .OnChange(state =>{ 
+  	DisplayMode = state.newValue;
+	if(DisplayMode === 0){
+	   let SafeDistanceUI = Menu.AddSlider(path_, 'Safe Distance (% Attack Range)', 1, 100, 100)
 	.OnChange(state => SafeDistanceUI = state.newValue)
 	.GetValue();
+	}
+  })
+  .GetValue();
+  
+
   
 Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
 
