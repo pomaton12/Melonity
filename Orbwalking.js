a1b756210f2e7bdc10b4d94c53b53f4e5da46e7e
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-24 06:10:18 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -93,7 +93,7 @@ HitRunHeros.OnUpdate = () => {
           const attackTime = 1 / attackSpeed;
 	  
 	  // Obtener el tipo de ataque del héroe
-	  const attackCapability = Entities.GetAttackCapability(localHero);
+	  const attackCapability = localHero.GetAttackCapability();
 	  console.log(' tipo de ataque del héroe',attackCapability);
 	  
 	  if (dist > attackRange) {
