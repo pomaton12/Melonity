08dcbffb6e2ab6824666ee49e1790621f8204048
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-25 05:26:42 -0500

    Update HabilidadShardWindrunner.js

--- HabilidadShardWindrunner.js
+++ HabilidadShardWindrunner.js
@@ -77,11 +77,11 @@ eval(`
                         let enemyDirection = vec2.sub(vec1).Normalized();
 
                         // Calcular la dirección opuesta
-                        let oppositeDirection = enemyDirection * -1;
+                        let oppositeDirection = enemyDirection.clone().multiplyScalar(-1);
 
                         // Lanzar Gale Force en la dirección opuesta desde la posición del héroe enemigo
                         let enemyPosition = enemy.GetAbsOrigin();
-                        let pushPosition = enemyPosition.add(oppositeDirection.multiplyScalar(500));
+                        let pushPosition = enemyPosition.clone().add(oppositeDirection.clone().multiplyScalar(500));
 
 			// Agregar condición para evitar lanzar gale force si el enemigo tiene activado bkb
 			if (enemy.HasModifier("modifier_black_king_bar_immune") === false) {
