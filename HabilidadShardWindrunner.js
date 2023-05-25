5fc75b7a556726dbd2e825d35dacfc2c46d84c59
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-25 14:44:35 -0500

    Update HabilidadShardWindrunner.js

--- HabilidadShardWindrunner.js
+++ HabilidadShardWindrunner.js
@@ -98,7 +98,7 @@ eval(`
 
 		        // Calcular la dirección opuesta
 		        const enemyPosition = enemy.GetAbsOrigin();
-		        const oppositeDirection = enemyDirection.Negated();
+		        const oppositeDirection = enemyDirection.mul(-1);
 
 		        console.log("Opositindirection", oppositeDirection);
 
