608cb2bdf3c29d8ef78ba6d4a350653d3da8d013
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-26 18:22:19 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -7,142 +7,100 @@
   \**********************************/
 /***/ (() => {
 
-eval(`
+//eval(`
 
-const HitRunHeros = {};
+	const HitRunHeros = {};
 
-let localHero;
-let myPlayer;
-let attackTarget;
-let EnemyHerotest;
+	let localHero;
+	let myPlayer;
+	let attackTarget;
+	let EnemyHerotest;
 
-const path_ = ['Heroes', 'Orbwalking'];
+	const path_ = ['Heroes', 'Orbwalking'];
 
-let isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true);
+	let isUiEnabled1 = Menu.AddToggle(path_, 'Orbwalking Enable', true);
 
-let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KEY_NONE);
+	let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KEY_NONE);
 
-let isUiEnabled2 = Menu.AddToggle(path_, 'Kill Safe Pos', true);
+	let isUiEnabled2 = Menu.AddToggle(path_, 'Kill Safe Pos', true);
 
-let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'],0)
-  .OnChange(state =>{   	
-	DisplayMode = state.newValue;
-  })
-  .GetValue();
-  
+	let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'],0)
+		.OnChange(state =>{   	
+		DisplayMode = state.newValue;
+		})
+		.GetValue();
 
-let SafeDistanceUI = Menu.AddSlider(path_, 'Safe Distance (% Attack Range)', 1, 100, 100)
-.OnChange(state => SafeDistanceUI = state.newValue)
-.GetValue();
-  
-Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
 
-//=========================================
-//       Evaluar Funciones
-//=========================================
+	let SafeDistanceUI = Menu.AddSlider(path_, 'Safe Distance (% Attack Range)', 1, 100, 100)
+		.OnChange(state => SafeDistanceUI = state.newValue)
+		.GetValue();
 
-// 2 .- ====   Funcion para calcular distancia2D
-function Dist2D(vec1, vec2) {
-if (vec1 && vec2) {
-    let pos1 = (vec1.x ? (vec1) : (vec1.GetAbsOrigin ? (vec1.GetAbsOrigin()) : (0)));
-    let pos2 = (vec2.x ? (vec2) : (vec2.GetAbsOrigin ? (vec2.GetAbsOrigin()) : (0)));
-    return pos1 && pos2 && pos1.sub(pos2).Length2D();
-}
-    return -1;
-}
+	Menu.GetFolder(['Heroes', 'Orbwalking']).SetImage('panorama/images/hud/reborn/icon_damage_psd.vtex_c');
 
-// 3 .- ====   Funcion POSICION DEL ANGULO
-function IsntUndefined(value, withfalse) {
-	return withfalse ? (value !== false) : value !== undefined && value !== null;
-}
-function GetAngleToPos(_e1, _e2, prefer = _e2, inrad) {
-	let [a, b] = [IsntUndefined(_e1.x) ? _e1 : _e1.GetAbsOrigin(), IsntUndefined(_e2.x) ? _e2 : _e2.GetAbsOrigin()];
-	if (prefer == _e1) {
-	    [a, b] = [b, a];
-	}
-	let atan2 = Math.atan2(b.y - a.y, b.x - a.x);
-	return inrad ? atan2 : (atan2 * (180 / Math.PI));
-}
+	//=========================================
+	//       Evaluar Funciones
+	//=========================================
 
-//=====================
-HitRunHeros.OnUpdate = () => {
-  if (localHero && isUiEnabled1.GetValue()) {
-    
-	if (localHero.IsAttacking()) {
-	  const localHeroPosition = localHero.GetAbsOrigin();
-	  const enemy  = EntitySystem.GetHeroesList().filter(hero => hero.GetTeamNum() !== localHero.GetTeamNum() && hero.IsAlive() && localHeroPosition.Distance(hero.GetAbsOrigin()) <= 1000);
-          const EnemyHero = enemy.reduce((closest, hero) => closest ? (localHeroPosition.Distance(hero.GetAbsOrigin()) < localHeroPosition.Distance(closest.GetAbsOrigin()) ? hero : closest) : hero, null);
-          const attackRange = localHero.GetAttackRange();
-	  const enemyHeroPosition = EnemyHero.GetAbsOrigin();
-	  const dist = Dist2D(localHero.GetAbsOrigin(), EnemyHero.GetAbsOrigin());
-	  
-	  const newRange = attackRange - attackRange * (SafeDistanceUI / 100);
-	  //localHero.ScriptAttackRange = 50;
-	  
-
-	  //const attackSpeed = localHero.GetAttacksPerSecond();
-          //const attackTime = 1 / attackSpeed;
-	  
-	  
-	  
-	  if (dist > attackRange) {
-	  
-	      if ( !isUiEnabled2.GetValue()) {
-	        if(newRange > 0){
-	         const pos = localHeroPosition.add(new Vector(newRange).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
-		  myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);		     
-		 setTimeout(function() {
-		    localHero.MoveTo(pos);
-		 }, 200); 
-		 myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);		     
-		
-	        }	 
-	      }
-	  } else {
-	  
-              if ( isUiEnabled2.GetValue() ) {
-	         const pos = localHeroPosition.add(new Vector(-150).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
-		 //localHero.MoveTo(pos);
-		 setTimeout(function() {
-		     myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, pos, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
-	         }, 300); 
-	     }
-	  }
-	  
-	  if (DisplayMode === 0) {
-
-	  } else if (DisplayMode === 1) {
-	      const mousePos = Input.GetWorldCursorPos();
-	      setTimeout(function() {localHero.MoveTo(mousePos);}, 100);
-	  }
+	// 2 .- ====   Funcion para calcular distancia2D
+	function Dist2D(vec1, vec2) {
+		if (vec1 && vec2) {
+			let pos1 = (vec1.x ? (vec1) : (vec1.GetAbsOrigin ? (vec1.GetAbsOrigin()) : (0)));
+			let pos2 = (vec2.x ? (vec2) : (vec2.GetAbsOrigin ? (vec2.GetAbsOrigin()) : (0)));
+			return pos1 && pos2 && pos1.sub(pos2).Length2D();
+		}
+		return -1;
 	}
 
+	// 3 .- ====   Funcion POSICION DEL ANGULO
+	function IsntUndefined(value, withfalse) {
+		return withfalse ? (value !== false) : value !== undefined && value !== null;
+	}
 	
-    if (KeyBindOrbwalk.IsKeyDown()) {
-      const mousePos = Input.GetWorldCursorPos();
-      const enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
-      enemies.sort((a, b) => {
-        const distA = a.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-        const distB = b.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
-        return distA - distB;
-      });
+	function GetAngleToPos(_e1, _e2, prefer = _e2, inrad) {
+		let [a, b] = [IsntUndefined(_e1.x) ? _e1 : _e1.GetAbsOrigin(), IsntUndefined(_e2.x) ? _e2 : _e2.GetAbsOrigin()];
+		if (prefer == _e1) {
+			[a, b] = [b, a];
+		}
+		let atan2 = Math.atan2(b.y - a.y, b.x - a.x);
+		return inrad ? atan2 : (atan2 * (180 / Math.PI));
+	}
 
-      let target = null;
-      for (const enemy of enemies) {
-        const dist = enemy.GetAbsOrigin().Distance(mousePos);
-        if (dist <= 100 || target == null) {
-          target = enemy;
-        }
-      }
+	//=====================
+	HitRunHeros.OnUpdate = () => {
+	if (localHero && isUiEnabled1.GetValue()) {
 
-      if (target != null) {
-	//localHero.AttackTarget(target);
-	let [order, targetHero, pos] = [Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, target, target.GetAbsOrigin()];
-	//myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, Input.GetWorldCursorPos(), null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
-        myPlayer.PrepareUnitOrders(order, targetHero, pos, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
-      }
-    }
-  }
+
+
+
+		if (KeyBindOrbwalk.IsKeyDown()) {
+			const mousePos = Input.GetWorldCursorPos();
+			const enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
+			
+			enemies.sort((a, b) => {
+				const distA = a.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
+				const distB = b.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
+				return distA - distB;
+			});
+
+			let target = null;
+			for (const enemy of enemies) {
+				const dist = enemy.GetAbsOrigin().Distance(mousePos);
+				if (dist <= 100 || target == null) {
+					target = enemy;
+				}
+			}
+			
+			
+			if (Engine.OnceAt(0.2)) {
+				if (target != null) {
+					//localHero.AttackTarget(target);
+					let [order, targetHero, pos] = [Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, target, target.GetAbsOrigin()];
+					//myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, Input.GetWorldCursorPos(), null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
+					myPlayer.PrepareUnitOrders(order, targetHero, pos, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
+				}
+			}	
+		}
+	}
 };
 
 HitRunHeros.OnScriptLoad = HitRunHeros.OnGameStart = () => {
