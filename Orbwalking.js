6e53f6a27f6631df17a5fedbb370effe220fe0fb
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-27 04:37:39 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -108,7 +108,7 @@ eval(`
 						newRange =  55;
 					}
 			
-					if (dist >= newRange) {
+					if (dist > newRange) {
 
 						if ( !isUiEnabled2.GetValue()) {
 							console.log("time = ",attackTime);
@@ -119,11 +119,9 @@ eval(`
 									myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);		     
 
 								} else {
-									//myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);		     
 							
 									if (Engine.OnceAt(attackTime)) {
 										myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, pos1, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
-		
 									}
 									
 								}
@@ -132,11 +130,10 @@ eval(`
 					} else {
 
 						if ( isUiEnabled2.GetValue() ) {
-							pos1 = localHeroPosition.add(new Vector(-100).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
-							//localHero.MoveTo(pos1);
-							//setTimeout(function() {
-								//myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, pos, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
-							//}, 300); 
+							pos1 = localHeroPosition.add(new Vector(dist - newRange-100).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
+							if (Engine.OnceAt(attackTime)) {
+								myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, pos1, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
+							}
 						}
 					}
 			
