6c7bedea7d03b6077e3b49af8d5ae3a2f4704ace
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-27 06:00:45 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -98,7 +98,7 @@ eval(`
 					const EnemyHero = target;
 					const attackRange = localHero.GetAttackRange();
 					const enemyHeroPosition = EnemyHero.GetAbsOrigin();
-					const dist = localHeroPosition.Distance(enemyHeroPosition);
+					const dist = localHeroPosition.Distance(enemyHeroPosition)-7;
 					const attackSpeed = localHero.GetAttacksPerSecond();
          				const attackTime = 1 / attackSpeed;
 					
@@ -113,7 +113,7 @@ eval(`
 						if ( !isUiEnabled2.GetValue()) {
 							console.log("dist = ",dist," dist = ",newRange);
 							if(newRange){
-								pos1 = localHeroPosition.add(new Vector(dist - newRange-7).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
+								pos1 = localHeroPosition.add(new Vector(dist - newRange).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
 
 								if (newRange == parseInt(dist)){
 									myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);		     
