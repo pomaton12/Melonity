3bfc695c65bc2d926946de87d3610cd952cf99b5
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-29 01:13:37 -0500

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,1 +1,147 @@
+/******/ (() => { // webpackBootstrap 
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/BestAutoLastHits.ts":
+/*!**********************************!*\
+  !*** ./src/BestAutoLastHits.ts ***!
+  \**********************************/
+/***/ (() => {
+
+//eval(`
+
+	const BestAutoLastHits = {};
+
+	let localHero;
+	let myPlayer;
+
+	const path_ = ['Creeps', 'Best AutoLastHit'];
+
+	let enableToggle = Menu.AddToggle(path_, 'Enable', true);
+	
+	let KeyBindLastHit = Menu.AddKeyBind(path_, 'AutoLastHits', Enum.ButtonCode.KEY_NONE);
+	
+	let DisplayModeMove = Menu.AddComboBox(path_, 'Move', ['Holt key', 'One Key'],0)
+		.OnChange(state =>{   	
+		DisplayModeMove = state.newValue;
+		})
+		.GetValue();
+	
+	let DisplayModeHitCreep = Menu.AddComboBox(path_, 'Hit Creeps', ['Enemy creeps', 'Ally creeps', 'Both'],0)
+		.OnChange(state =>{   	
+		DisplayModeHitCreep = state.newValue;
+		})
+		.GetValue();
+	
+	let DisplayModeHitEnemy = Menu.AddComboBox(path_, 'Hit Enemies', ['No hit', 'without creep aggro', 'Aggression mode'],0)
+		.OnChange(state =>{   	
+		DisplayModeHitEnemy = state.newValue;
+		})
+		.GetValue();
+
+
+	function getClosestEnemyHero(radius) {
+	    const enemyHeroes = EntitySystem.GetHeroesList().filter(
+		(hero) => !hero.IsIllusion() && !hero.IsMeepoClone() && !hero.IsSameTeam(localHero) && hero.IsAlive() && !hero.IsIllusion()
+	    );
+	    
+	    if (!enemyHeroes || enemyHeroes.length <= 0) {
+		return;
+	    }
+
+	    let closestHero = null;
+	    let closestDistance = Number.MAX_VALUE;
+
+	    for (const hero of enemyHeroes) {
+		const distance = localHero.GetAbsOrigin().sub(hero.GetAbsOrigin()).Length2D();
+		if (distance < radius && distance < closestDistance) {
+		    closestHero = hero;
+		    closestDistance = distance;
+		}
+	    }
+
+	    return closestHero;
+	}
+
+	function SendOrderMovePos(vector) {
+	    myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, vector, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
+	}
+
+	function getClosestCreep(creeps, target) {
+	    let closestCreep = null;
+	    for (const creep of creeps) {
+		if (creep.IsCreep() && !creep.IsDormant() && creep.IsAlive()) {
+		    closestCreep = creep;
+		}
+	    }
+	    return closestCreep;
+	}
+
+	BestAutoLastHits.OnUpdate = () => {
+	    if (!localHero || !enableToggle.GetValue()) {
+		return;
+	    }
+
+	    const attackRadius = 500;
+
+	    if (DisplayModeHitEnemy === 0) {
+			const closestEnemyHero = getClosestEnemyHero(attackRadius);
+
+			if (closestEnemyHero) {
+				if (Engine.OnceAt(0.2)) {
+					myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, closestEnemyHero, null, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, localHero, false, true);
+				}
+			}
+	    }
+
+	    if (DisplayModeHitCreep === 0) {
+			const closestEnemyHero = getClosestEnemyHero(attackRadius);
+
+			if (!closestEnemyHero) {
+				const laneCreeps = localHero.GetUnitsInRadius(2000, Enum.TeamType.TEAM_ENEMY);
+			
+				if (laneCreeps || laneCreeps.length  > 0) {
+
+					const closestLaneCreep = getClosestCreep(laneCreeps, localHero.GetAbsOrigin());
+
+					if (closestLaneCreep) {
+
+						if (Engine.OnceAt(0.2)) {
+						myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_MOVE, null, closestLaneCreep.GetAbsOrigin(), null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, localHero, false, true);
+						}
+
+					}
+				} else {
+					return;
+				}											
+			}
+	    }
+	};
+
+	BestAutoLastHits.OnScriptLoad = BestAutoLastHits.OnGameStart = () => {
+	    localHero = EntitySystem.GetLocalHero();
+	    myPlayer = EntitySystem.GetLocalPlayer();
+	};
+
+	BestAutoLastHits.OnGameEnd = () => {
+	    localHero = null;
+	    myPlayer = null;
+	};
+
+	RegisterScript(BestAutoLastHits);
+
+
+
+`);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/BestAutoLastHits.ts"]();
+/******/ 	
+/******/ })()
+;
 
