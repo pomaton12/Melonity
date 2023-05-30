03cf6514f4f9165d7c3bbf2e6adb9987063d531f
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-30 11:37:07 -0500

    Update Alche.js

--- Alche.js
+++ Alche.js
@@ -1,11 +1,3 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
@@ -55,10 +47,10 @@ eval(`
                         const bkb = localHero.GetItem('item_black_king_bar', true);
                         if (itemSelection[1] && manta && manta.GetCooldown() === 0) {
                             manta.CastNoTarget();
-                            return;
+                            break;
                         } else if (itemSelection[0] && (!manta || manta.GetCooldown() > 0) && bkb && bkb.GetCooldown() === 0) {
                             bkb.CastNoTarget();
-                            return;
+                            break;
                         }
                     }
                 }
