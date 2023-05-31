6ae9e698c30fa1c989b34c993fd1d3a1931a4d81
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-31 03:20:45 -0500

    Update HabilidadShardWindrunner.js

--- HabilidadShardWindrunner.js
+++ HabilidadShardWindrunner.js
@@ -48,7 +48,7 @@ eval(`
 					let enemyPosition = enemy.GetAbsOrigin();
 					let enemyDirection = enemyPosition.sub(herolPosition);
 
-					let VisionNPC = NPC.FindFacingNPC(enemy);
+					let VisionNPC = enemy.FindFacingNPC();
 
 					if(VisionNPC == localHero){
 						enemyDirection = herolPosition.sub(enemyPosition);
