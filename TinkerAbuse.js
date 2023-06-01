c18942c2d303c6665cbd5e20eb9a1aecc8ccc928
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-01 11:17:22 -0500

    Update TinkerAbuse.js

--- TinkerAbuse.js
+++ TinkerAbuse.js
@@ -32,15 +32,34 @@ eval(`
 	TinkerLastHit.OnUpdate = () => {
 		
 		if (localHero && MissileToggle.GetValue()) {
-			if (localHero.GetUnitName() !== "npc_dota_hero_windrunner") {
+			if (localHero.GetUnitName() !== "npc_dota_hero_tinker") {
 				return;
 			}
-			
-			
-			
-			
-		}
 
+			// Check if Blink Dagger was used
+			const blinkDagger = localHero.GetItemByName("item_blink");
+			if (blinkDagger && blinkDagger.IsCooldownReady()) {
+				const currentTime = GameRules.GetGameTime();
+				const lastBlinkTime = localHero.GetLastAbilityCastTime("tinker_blink");
+
+				// Check if less than 1 second has passed since Blink Dagger was used
+				if (currentTime - lastBlinkTime <= 1) {
+					// Find visible enemy heroes within 2000 radius
+					const enemies = EntitySystem.GetHeroesList().filter(hero => {
+						return hero.IsAlive() && hero.IsVisible() && hero.IsEnemy(localHero) && hero.GetRangeTo(localHero) <= 2000;
+					});
+
+					if (enemies.length > 0) {
+						// Cast Tinker's second ability on the first enemy hero found
+						const missileAbility = localHero.GetAbilityByName("tinker_heat_seeking_missile");
+						if (missileAbility && missileAbility.IsCooldownReady()) {
+							const target = enemies[0];
+							localHero.CastAbilityNoTarget(missileAbility);
+						}
+					}
+				}
+			}
+		}
 	};
 
 
