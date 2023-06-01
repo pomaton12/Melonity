7788192c5c0f6fdd3bd6cfe453d033d170dea72b
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-01 00:47:07 -0500

    Update IlusionAgresive.js

--- IlusionAgresive.js
+++ IlusionAgresive.js
@@ -21,7 +21,7 @@ eval(`
 	let attackHeroToggle = Menu.AddToggle(path_, 'Attack Hero', true);
 	attackHeroToggle.SetImage('panorama/images/spellicons/spectre_haunt_png.vtex_c');
 	let pushLineCreepsToggle = Menu.AddToggle(path_, 'Push Line Creeps', true);
-	pushLineCreepsToggle.SetImage('models/creeps/lane_creeps/creep_radiant_ranged/radiant_ranged_mushroom_normal_psd_3a07aa02.vtex_c');
+	pushLineCreepsToggle.SetImage('panorama/images/econ/creeps/lane_creeps/creep_2021_dire/dire_2021_radiant_creeps_png.vtex_c');
 
 function getIllusions() {
     if (illusionList.length < 5) {
