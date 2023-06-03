233a19f8d160767c9e659dffd589bb19055b7551
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-03 15:00:48 -0500

    Update KunkkaScrip.js

--- KunkkaScrip.js
+++ KunkkaScrip.js
@@ -1,11 +1,3 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
@@ -16,107 +8,110 @@
 /***/ (() => {
 
 eval(`
-  const AutoStealKunkka = {};
-  let local;
-  let localPlayer;
-  let TorrentStorm;
-  let TidalWave;
-  let enemyHeroes;
-  let torrentStormTimer = null;
-  let lastUltimateCastTime = 0;
-  let tidalWaveCastAfterUltimate = false;
-  
-  const path_ = ['Heroes', 'Strength', 'Kunkka'];
-  let isUiEnabled1 = Menu.AddToggle(path_, 'Torrent Storm Use', true);
-  isUiEnabled1.SetImage('panorama/images/spellicons/kunkka_torrent_storm_png.vtex_c');
-  
-  let isUiEnabled2 = Menu.AddToggle(path_, 'Tidal Wave Use', true);
-  isUiEnabled2.SetImage('panorama/images/spellicons/kunkka_tidal_wave_png.vtex_c');
+	const AutoStealKunkka = {};
+	let local;
+	let localPlayer;
+	let TorrentStorm;
+	let TidalWave;
+	let enemyHeroes;
+	let torrentStormTimer = null;
+	let lastUltimateCastTime = 0;
+	let tidalWaveCastAfterUltimate = false;
 
-  AutoStealKunkka.OnUpdate = () => {
-    if (localHero && isUiEnabled1.GetValue()) {
-        // Verifica si el héroe local es Kunkka
-        if (localHero.GetUnitName() !== "npc_dota_hero_kunkka")
-            return;
+	const path_ = ['Heroes', 'Strength', 'Kunkka'];
+	let isUiEnabled1 = Menu.AddToggle(path_, 'Torrent Storm Use', true);
+	isUiEnabled1.SetImage('panorama/images/spellicons/kunkka_torrent_storm_png.vtex_c');
 
-        // Si no se ha inicializado TorrentStorm, obtén la habilidad Torrent Storm de Kunkka
-        if (!TorrentStorm) {
-            TorrentStorm = localHero.GetAbilityByIndex(3);
-        }
+	let isUiEnabled2 = Menu.AddToggle(path_, 'Tidal Wave Use', true);
+	isUiEnabled2.SetImage('panorama/images/spellicons/kunkka_tidal_wave_png.vtex_c');
 
-        // Obtén la habilidad ultimate de Kunkka (Ghostship)
-        const Ghostship = localHero.GetAbilityByIndex(5);
+	AutoStealKunkka.OnUpdate = () => {
+		if (localHero && isUiEnabled1.GetValue()) {
+			// Verifica si el héroe local es Kunkka
+			if (localHero.GetUnitName() !== "npc_dota_hero_kunkka")
+				return;
 
-        // Verifica si el ultimate de Kunkka está en cooldown y si no hay un temporizador activo
-        if (Ghostship && Ghostship.GetCooldown() > 0 && !torrentStormTimer) {
-            // Genera un tiempo aleatorio entre 0 y 3000 milisegundos (3 segundos)
-            const randomDelay = Math.floor(Math.random() * 3000);
+			// Si no se ha inicializado TorrentStorm, obtén la habilidad Torrent Storm de Kunkka
+			if (!TorrentStorm) {
+				TorrentStorm = localHero.GetAbilityByIndex(3);
+			}
 
-            // Establece un temporizador para activar Torrent Storm dentro de los 3 segundos después
-            torrentStormTimer = setTimeout(() => {
-                // Si Torrent Storm se puede lanzar, actívalo
-                if (TorrentStorm.CanCast()) {
-                    TorrentStorm.CastPosition(localHero.GetAbsOrigin());
-                }
-                // Limpia el temporizador
-                clearTimeout(torrentStormTimer);
-                torrentStormTimer = null;
-            }, randomDelay);
-        }
-    }
-if (localHero && isUiEnabled2.GetValue()) {
-    if (localHero.GetUnitName() !== "npc_dota_hero_kunkka")
-        return;
+			// Obtén la habilidad ultimate de Kunkka (Ghostship)
+			const Ghostship = localHero.GetAbilityByIndex(5);
 
-    let TidalWave = localHero.GetAbilityByIndex(4);
- if (!TidalWave || localHero.HasModifier("modifier_item_invisibility_edge_windwalk") || localHero.HasModifier("modifier_item_silver_edge_windwalk")) {
-        return;
-    }
+			// Verifica si el ultimate de Kunkka está en cooldown y si no hay un temporizador activo
+			if (Ghostship && Ghostship.GetCooldown() > 0 && !torrentStormTimer) {
+				// Genera un tiempo aleatorio entre 0 y 3000 milisegundos (3 segundos)
+				const randomDelay = Math.floor(Math.random() * 3000);
 
-    const localHeroHealthPercentage = (localHero.GetHealth() / localHero.GetMaxHealth()) * 100;
-    const localHeroPosition = localHero.GetAbsOrigin();
-    const enemyHeroes = EntitySystem.GetHeroesList().filter(hero => hero.GetTeamNum() !== localHero.GetTeamNum() && hero.IsAlive() && !hero.HasModifier("modifier_black_king_bar_immune") && localHeroPosition.Distance(hero.GetAbsOrigin()) <= 749);
-    const closestEnemyHero = enemyHeroes.reduce((closest, hero) => closest ? (localHeroPosition.Distance(hero.GetAbsOrigin()) < localHeroPosition.Distance(closest.GetAbsOrigin()) ? hero : closest) : hero, null);
+				// Establece un temporizador para activar Torrent Storm dentro de los 3 segundos después
+				torrentStormTimer = setTimeout(() => {
+					// Si Torrent Storm se puede lanzar, actívalo
+					if (TorrentStorm.CanCast()) {
+						TorrentStorm.CastPosition(localHero.GetAbsOrigin());
+					}
+					// Limpia el temporizador
+					clearTimeout(torrentStormTimer);
+					torrentStormTimer = null;
+				}, randomDelay);
+			}
+		}
+		
+		if (localHero && isUiEnabled2.GetValue()) {
+			if (localHero.GetUnitName() !== "npc_dota_hero_kunkka")
+				return;
 
-    if (closestEnemyHero && TidalWave.CanCast() && !tidalWaveCastAfterUltimate) {
-        const enemyHeroPosition = closestEnemyHero.GetAbsOrigin();
-        const direction = (enemyHeroPosition.sub(localHeroPosition)).Normalized();
-        if (localHeroHealthPercentage < 30) {
-            castPosition = localHeroPosition - (direction * 300);
-            TidalWave.CastPosition(closestEnemyHero.GetAbsOrigin());
-        } else {
-            castPosition = localHeroPosition + (direction * 300);
-            TidalWave.CastPosition(localHero.GetAbsOrigin());
-        }
-        tidalWaveCastAfterUltimate = true;
-    }
+			let TidalWave = localHero.GetAbilityByIndex(4);
+			if (!TidalWave || localHero.HasModifier("modifier_item_invisibility_edge_windwalk") || localHero.HasModifier("modifier_item_silver_edge_windwalk")) {
+				return;
+			}
 
-    const ultimateAbility = localHero.GetAbilityByIndex(5);
-    if (ultimateAbility && ultimateAbility.IsActivated() && TidalWave.CanCast()) {
-        const currentTime = GameRules.GetGameTime();
-        if (currentTime - lastUltimateCastTime > 3.1) {
-            tidalWaveCastAfterUltimate = false;
-            lastUltimateCastTime = currentTime;
-        }
-    }
-}
+			const localHeroHealthPercentage = (localHero.GetHealth() / localHero.GetMaxHealth()) * 100;
+			const localHeroPosition = localHero.GetAbsOrigin();
+			const enemyHeroes = EntitySystem.GetHeroesList().filter(hero => hero.GetTeamNum() !== localHero.GetTeamNum() && hero.IsAlive() && !hero.HasModifier("modifier_black_king_bar_immune") && localHeroPosition.Distance(hero.GetAbsOrigin()) <= 749);
+			const closestEnemyHero = enemyHeroes.reduce((closest, hero) => closest ? (localHeroPosition.Distance(hero.GetAbsOrigin()) < localHeroPosition.Distance(closest.GetAbsOrigin()) ? hero : closest) : hero, null);
 
-  };
+			if (closestEnemyHero && TidalWave.CanCast() && !tidalWaveCastAfterUltimate) {
+				const enemyHeroPosition = closestEnemyHero.GetAbsOrigin();
+				const direction = (enemyHeroPosition.sub(localHeroPosition)).Normalized
+				const IdealPosition;
+				
+				if (localHeroHealthPercentage < 30) {
+					IdealPosition = localHeroPosition - (direction.mul(new Vector(300, 300, 0)));
+					TidalWave.CastPosition(IdealPosition);
+				} else {
+					IdealPosition = localHeroPosition + (direction.mul(new Vector(300, 300, 0)));
+					TidalWave.CastPosition(IdealPosition);
+				}
+				tidalWaveCastAfterUltimate = true;
+			}
 
-  AutoStealKunkka.OnScriptLoad = AutoStealKunkka.OnGameStart = () => {
-      localPlayer = EntitySystem.GetLocalPlayer();
-      localHero = EntitySystem.GetLocalHero();
-  };
+			const ultimateAbility = localHero.GetAbilityByIndex(5);
+			if (ultimateAbility && ultimateAbility.IsActivated() && TidalWave.CanCast()) {
+				const currentTime = GameRules.GetGameTime();
+				if (currentTime - lastUltimateCastTime > 3.1) {
+					tidalWaveCastAfterUltimate = false;
+					lastUltimateCastTime = currentTime;
+				}
+			}
+		}
 
-  AutoStealKunkka.OnGameEnd = () => {
-      localPlayer = null;
-      localHero = null;
-      TorrentStorm = null;
-      TidalWave = null;
+	};
 
-  };
+	AutoStealKunkka.OnScriptLoad = AutoStealKunkka.OnGameStart = () => {
+		localPlayer = EntitySystem.GetLocalPlayer();
+		localHero = EntitySystem.GetLocalHero();
+	};
 
-  RegisterScript(AutoStealKunkka);
+	AutoStealKunkka.OnGameEnd = () => {
+		localPlayer = null;
+		localHero = null;
+		TorrentStorm = null;
+		TidalWave = null;
+
+	};
+
+	RegisterScript(AutoStealKunkka);
 `);
 /***/ })
 
