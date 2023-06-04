30bca2eefa533b75b8af49fe0f5fe9622aaebebe
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-04 04:27:17 -0500

    Update espacio.js

--- espacio.js
+++ espacio.js
@@ -26,6 +26,31 @@ eval(`
 	Menu.GetFolder(["Heroes", "Intelligence", "Nature's Prophet","Unit Attack Block"]).SetImage('panorama/images/spellicons/furion_force_of_nature_png.vtex_c');
 
 	const MIN_DISTANCE_TO_BLOCK = 100; // Distancia mínima para bloquear al enemigo
+	
+	
+	function getClosestEnemyHero(radius) {
+	    const enemyHeroes = EntitySystem.GetHeroesList().filter(
+		(hero) => !hero.IsIllusion() && !hero.IsMeepoClone() && !hero.IsSameTeam(localHero) && hero.IsAlive() && !hero.IsIllusion()
+	    );
+	    
+	    if (!enemyHeroes || enemyHeroes.length <= 0) {
+		return;
+	    }
+
+	    let closestHero = null;
+	    let closestDistance = Number.MAX_VALUE;
+
+	    for (const hero of enemyHeroes) {
+		const distance = localHero.GetAbsOrigin().sub(hero.GetAbsOrigin()).Length2D();
+		if (distance < radius && distance < closestDistance) {
+		    closestHero = hero;
+		    closestDistance = distance;
+		}
+	    }
+
+	    return closestHero;
+	}
+	
 
 	FurionTrendAbuse.OnUpdate = () => {
 		if (localHero && isUiEnabled.GetValue()) {
@@ -44,6 +69,7 @@ eval(`
 					furionUnits.forEach((unit) => {
 						console.log("Unidad",unit);
 						if(unit && unit.IsAlive() && !unit.IsDormant() && localHero.GetOwner(unit)){
+							const target = getClosestEnemyHero(700);
 							const targetPos = target.GetAbsOrigin();
 							const unitPos = unit.GetAbsOrigin();
 							const distanceToTarget = unitPos.Distance(targetPos);
