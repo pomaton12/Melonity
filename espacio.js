152a81a94161cb9c7ee60200835147b07844babf
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-04 05:43:38 -0500

    Update espacio.js

--- espacio.js
+++ espacio.js
@@ -8,7 +8,6 @@
 /***/ (() => {
 
 eval(`
-
 	const FurionTrendAbuse = {};
 
 	let localHero;
@@ -18,21 +17,19 @@ eval(`
 
 	const path_ = ["Heroes", "Intelligence", "Nature's Prophet","Unit Attack Block"];
 
-	// Creación del toggle isUiEnabled
 	let isUiEnabled = Menu.AddToggle(path_, 'Enable', true);
-	
+
 	let KeyBindOrder = Menu.AddKeyBind(path_, 'Key of Combo', Enum.ButtonCode.KEY_NONE);
-	
+
 	Menu.GetFolder(["Heroes", "Intelligence", "Nature's Prophet","Unit Attack Block"]).SetImage('panorama/images/spellicons/furion_force_of_nature_png.vtex_c');
 
-	const MIN_DISTANCE_TO_BLOCK = 150; // Distancia mínima para bloquear al enemigo
-	
-	
+	const MIN_DISTANCE_TO_BLOCK = 150;
+
 	function getClosestEnemyHero(radius) {
 	    const enemyHeroes = EntitySystem.GetHeroesList().filter(
 		(hero) => !hero.IsIllusion() && !hero.IsMeepoClone() && !hero.IsSameTeam(localHero) && hero.IsAlive() && !hero.IsIllusion()
 	    );
-	    
+
 	    if (!enemyHeroes || enemyHeroes.length <= 0) {
 		return;
 	    }
@@ -50,54 +47,48 @@ eval(`
 
 	    return closestHero;
 	}
-	
+
+	function getFurionUnits() {
+	    const allUnits = EntitySystem.GetUnitsList();
+	    const furionUnits = allUnits.filter(unit => unit.GetUnitName() === "npc_dota_furion_treant" && unit.GetOwner() === localHero);
+	    return furionUnits;
+	}
 
 	FurionTrendAbuse.OnUpdate = () => {
-		if (localHero && isUiEnabled.GetValue()) {
+	    if (localHero && isUiEnabled.GetValue()) {
 
-			if (localHero.GetUnitName() !== "npc_dota_hero_furion") {
-				return;
-			}
-
-			if (KeyBindOrder.IsKeyDown()) {
-
-				const furionUnits = localHero.GetUnitsInRadius(500, Enum.TeamType.TEAM_FRIEND);
-				
-
-				if(furionUnits){
-
-					furionUnits.forEach((unit) => {
-						console.log("Unidad",unit);
-						if(unit && unit.IsAlive() && !unit.IsDormant() && localHero.GetOwner(unit)){
-							const target = getClosestEnemyHero(1000);
-							const targetPos = target.GetAbsOrigin();
-							const unitPos = unit.GetAbsOrigin();
-							const distanceToTarget = unitPos.Distance(targetPos);
-
-							//if (distanceToTarget < MIN_DISTANCE_TO_BLOCK) {
-								const dir = target.GetRotation().GetForward().Normalized();
-								const blockingPos = targetPos.add(dir.mul(new Vector(150, 150, 0)));
-								
-								if (Engine.OnceAt(0.2)) {
-									myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION,null,blockingPos,null,Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, unit, false, true);
-								}
-								
-							// else {
-								//unit.Attack(target, false);
-							//}
-						}
-					});
-					
-				}
-			
-			}
-		
+		if (localHero.GetUnitName() !== "npc_dota_hero_furion") {
+		    return;
 		}
-		
-		
+
+		if (KeyBindOrder.IsKeyDown()) {
+
+		    const furionUnits = getFurionUnits();
+
+		    if (furionUnits) {
+
+			furionUnits.forEach((unit) => {
+			    if (unit && unit.IsAlive() && !unit.IsDormant()) {
+				const target = getClosestEnemyHero(1000);
+				const targetPos = target.GetAbsOrigin();
+
+				const dir = target.GetRotation().GetForward().Normalized();
+				const blockingPos = targetPos.add(dir.mul(new Vector(150, 150, 0)));
+
+				if (Engine.OnceAt(0.2)) {
+				    myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, blockingPos, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, unit, false, true);
+				}
+
+			    }
+			});
+
+		    }
+
+		}
+
+	    }
+
 	};
-
-
 
 	FurionTrendAbuse.OnScriptLoad = FurionTrendAbuse.OnGameStart = () => {
 	    localHero = EntitySystem.GetLocalHero();
@@ -110,6 +101,7 @@ eval(`
 	};
 
 	RegisterScript(FurionTrendAbuse);
+
 
 
 
