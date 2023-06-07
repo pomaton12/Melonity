8aacdef27637e9276c1e0daaaa7e8275d2339ea5
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-07 05:30:40 -0500

    Update BestLastHits.js

--- BestLastHits.js
+++ BestLastHits.js
@@ -223,7 +223,7 @@ eval(`
 						console.log("unit = ",nearest_enemi);
 					//} else {
 						if (Engine.OnceAt(0.1)) {
-							//myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, closestCreep, null, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, localHero, false, true);
+							myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, nearest_enemi, null, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, localHero, false, true);
 
 							SendOrderMovePos(Input.GetWorldCursorPos());
 						}
