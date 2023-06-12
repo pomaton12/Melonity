a642d5c4fd05ef8ba0f738ce83f52ba2c906fdf7
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-12 16:03:59 -0500

    Update HabilidadShardWindrunner.js

--- HabilidadShardWindrunner.js
+++ HabilidadShardWindrunner.js
@@ -84,107 +84,95 @@ eval(`
 	  }
 	
 	
-		if (localHero && isUiEnabled.GetValue()) {
-			if (localHero.GetUnitName() !== "npc_dota_hero_windrunner") {
-				return;
-			}
+	  if (localHero && isUiEnabled.GetValue()) {
+	    if (localHero.GetUnitName() !== "npc_dota_hero_windrunner") {
+	      return;
+	    }
 
-			const modifiers = localHero.GetModifiers();
-			for (let modifier of modifiers) {
-				if (modifier.GetName() === 'modifier_windrunner_focusfire') {
-					const remainingTime = modifier.GetRemainingTime();
-					if (remainingTime <= 20) {
-						// Nueva condición para activar windrun siempre
-						let windrun = localHero.GetAbilityByIndex(2);
-						if (windrun && windrun.IsExist() && windrun.CanCast()) {
-							windrun.CastNoTarget();
-						}
-						// Nueva condición para activar BKB si el enemigo tiene activado Blade Mail
-						let enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
+	    const modifiers = localHero.GetModifiers();
+	    for (let modifier of modifiers) {
+	      if (modifier.GetName() === 'modifier_windrunner_focusfire') {
+		const remainingTime = modifier.GetRemainingTime();
+		if (remainingTime <= 20) {
+		  // Nueva condición para activar windrun siempre
+		  let windrun = localHero.GetAbilityByIndex(2);
+		  if (windrun && windrun.IsExist() && windrun.CanCast()) {
+		    windrun.CastNoTarget();
+		  }
+		  // Nueva condición para activar BKB si el enemigo tiene activado Blade Mail
+		  let enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
 
-						if (enemies.length >= 3) {
-							let bkb = localHero.GetItem('item_black_king_bar', true);
-							if (bkb && bkb.CanCast()) {
-								bkb.CastNoTarget();
-							}
-						}
+		  if (enemies.length >= 3) {
+		    let bkb = localHero.GetItem('item_black_king_bar', true);
+		    if (bkb && bkb.CanCast()) {
+		      bkb.CastNoTarget();
+		    }
+		  }
 
-						for (let enemy of enemies) {
-							if (enemy.HasModifier("modifier_item_blade_mail_reflect")) {
-								let bkb = localHero.GetItem('item_black_king_bar', true);
-								if (bkb && bkb.CanCast()) {
-									bkb.CastNoTarget();
-								}
-							}
+		  for (let enemy of enemies) {
+		    if (enemy.HasModifier("modifier_item_blade_mail_reflect")) {
+		      let bkb = localHero.GetItem('item_black_king_bar', true);
+		      if (bkb && bkb.CanCast()) {
+			bkb.CastNoTarget();
+		      }
+		    }
+		  }
 
-							if (enemy.HasModifier("modifier_black_king_bar_immune") === false ) {
-								let disperserI = localHero.GetItem('item_disperser', true);
-								let diffusalI = localHero.GetItem('item_diffusal_blade', true);
-								if (disperserI && disperserI.CanCast()) {
-									disperserI.CastNoTarget();
-								}
-								if (diffusalI && diffusalI.CanCast()) {
-									diffusalI.CastNoTarget();
-								}
+		  let gale_force = localHero.GetAbilityByIndex(3);
+		  let enemyPositions = {};
+		  if (gale_force && gale_force.IsExist() && gale_force.CanCast()) {
+		    enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
+		    for (let enemy of enemies) {
+		      let enemyId = enemy.GetPlayerID();
+		      
+		      let vec1 = localHero.GetAbsOrigin();
+		      let vec2 = enemy.GetAbsOrigin();
+		      let distance = vec1.sub(vec2).Length2D();
+		       
+		      if (distance <= 1000) {
+		      // Actualizar la posición inicial del enemigo en cada iteración
+		      enemyPositions[enemyId] = enemy.GetAbsOrigin();
 
-							}
-						}
+		      // Calcular la dirección en la que el enemigo está viendo
+		      let posINI = enemyPositions[enemyId];
+		      //let posFIN = enemy.GetAbsOrigin();
 
-						let gale_force = localHero.GetAbilityByIndex(3);
-						let enemyPositions = {};
-						if (gale_force && gale_force.IsExist() && gale_force.CanCast()) {
-							enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
-							for (let enemy of enemies) {
-								let enemyId = enemy.GetPlayerID();
+	          if (Engine.OnceAt(0.2)) {
+				posFIN = enemy.GetAbsOrigin();	      
+		      }
+			
+		      if (posINI.x === posFIN.x && posINI.y === posFIN.y) {
+				continue;
+		      }
 
-								let vec1 = localHero.GetAbsOrigin();
-								let vec2 = enemy.GetAbsOrigin();
-								let distance = vec1.sub(vec2).Length2D();
+		      const enemyDirection = (posFIN.sub(posINI)).Normalized();
 
-								if (distance <= 1000) {
-									// Actualizar la posición inicial del enemigo en cada iteración
-									enemyPositions[enemyId] = enemy.GetAbsOrigin();
+			enemyPositions[enemyId] = posFIN;
+						
+		        // Calcular la dirección opuesta
+		        const enemyPosition = enemy.GetAbsOrigin();
+		        const oppositeDirection = enemyDirection.mul(new Vector(-1, -1, -1));
 
-									// Calcular la dirección en la que el enemigo está viendo
-									let posINI = enemyPositions[enemyId];
-									//let posFIN = enemy.GetAbsOrigin();
-
-									if (Engine.OnceAt(0.2)) {
-										posFIN = enemy.GetAbsOrigin();	      
-									}
-
-									if (posINI.x === posFIN.x && posINI.y === posFIN.y) {
-										continue;
-									}
-
-									const enemyDirection = (posFIN.sub(posINI)).Normalized();
-
-									enemyPositions[enemyId] = posFIN;
-
-									// Calcular la dirección opuesta
-									const enemyPosition = enemy.GetAbsOrigin();
-									const oppositeDirection = enemyDirection.mul(new Vector(-1, -1, -1));
-
-									// Lanzar Gale Force en la dirección opuesta desde la posición del héroe enemigo
-									let pushPosition = enemyPosition.add(oppositeDirection.mul(new Vector(100, 100, 0)));
-									myPlayer.PrepareUnitOrders(30, null, enemyPosition, gale_force, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY, localHero);
-									// Agregar condición para evitar lanzar gale force si el enemigo tiene activado bkb
-									if (enemy.HasModifier("modifier_black_king_bar_immune") === false) {
-										gale_force.CastPosition(pushPosition);
-										setTimeout(function() {}, 300);
-									}
-								}
-							}
-						}
-
-						// Agregar un if statement para verificar si el modificador "modifier_windrunner_focusfire" ha terminado
-						if (!modifiers.some(modifier => modifier.GetName() === 'modifier_windrunner_focusfire')) {
-							break;
-						}
-					}
+		        // Lanzar Gale Force en la dirección opuesta desde la posición del héroe enemigo
+		        let pushPosition = enemyPosition.add(oppositeDirection.mul(new Vector(100, 100, 0)));
+				myPlayer.PrepareUnitOrders(30, null, enemyPosition, gale_force, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY, localHero);
+				// Agregar condición para evitar lanzar gale force si el enemigo tiene activado bkb
+				if (enemy.HasModifier("modifier_black_king_bar_immune") === false) {
+				  gale_force.CastPosition(pushPosition);
+				  setTimeout(function() {}, 300);
 				}
-			}
+		      }
+		    }
+		  }
+
+		  // Agregar un if statement para verificar si el modificador "modifier_windrunner_focusfire" ha terminado
+		  if (!modifiers.some(modifier => modifier.GetName() === 'modifier_windrunner_focusfire')) {
+		    break;
+		  }
 		}
+	      }
+	    }
+	  }
 	};
 
 	// Definición de la función OnScriptLoad
