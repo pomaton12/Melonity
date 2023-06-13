1cbb4eedde8f084d2d82b425ebdf20a6f58e3dda
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-13 11:30:59 -0500

    Update Nuevo.js

--- Nuevo.js
+++ Nuevo.js
@@ -37,7 +37,7 @@ eval(`
 				let abilityPS = localHero.GetAbilityByIndex(2); //monkey_king_primal_spring
 				let nCastRange = abilityTD.GetCastRange();
 				
-				let tableNearbyTrees = Trees.InRadius(localHero.GetAbsOrigin(), nCastRange );
+				let tableNearbyTrees = Tree.InRadius(localHero.GetAbsOrigin(), nCastRange );
 								
 				console.log("Arbol = ",tableNearbyTrees);
 			}
