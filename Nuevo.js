08632013e18000aaef76c6fd455a53e39cd88355
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-18 05:44:22 -0500

    Update Nuevo.js

--- Nuevo.js
+++ Nuevo.js
@@ -81,13 +81,13 @@ eval(`
 					safePosition = FuntainSafePos;
 				}
 				
-				myPlayer.PrepareUnitOrders( Enum.UnitOrder.DOTA_UNIT_ORDER_CAST_POSITION,null,safePosition,Ultimate,Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero);
+				myPlayer.PrepareUnitOrders( Enum.UnitOrder.DOTA_UNIT_ORDER_CAST_POSITION,localHero,safePosition,Ultimate,Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero);
 				//Ultimate.CastPosition(safePosition);
 				console.log("safe position",safePosition);
 				let tpitem = localHero.GetItem('item_tpscroll', true);
 				if (tpitem && tpitem.CanCast()) {
 					//myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_CAST_NO_TARGET,null,FuntainSafePos,tpitem,Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero);
-					tpitem.CastPosition(FuntainSafePos);
+					//tpitem.CastPosition(FuntainSafePos);
 				}
 			}
 	    }
