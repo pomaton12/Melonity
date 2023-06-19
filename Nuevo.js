0ef971163913ec3a2ad8d575b834864fd7e00a76
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-19 08:39:53 -0500

    Update Nuevo.js

--- Nuevo.js
+++ Nuevo.js
@@ -78,7 +78,7 @@ eval(`
 						let RefresherOrb = localHero.GetItem('item_refresher', true);
 
 						// Añade esta sección en tu bucle principal
-						if (AghanimsScepter || localHero.HasScepter()) { // Verifica si el héroe tiene el Cetro de Aghanim
+						if (AghanimsScepter || localHero.HasModifier("modifier_item_ultimate_scepter_consumed")) { // Verifica si el héroe tiene el Cetro de Aghanim
 						    let enemiesInVortexRange = localHero.GetHeroesInRadius(425, Enum.TeamType.TEAM_ENEMY);
 							if (enemiesInVortexRange.length > 2 && Ability2 && Ability2.CanCast()) {
 								Ability2.CastTarget(target);; // Cast Electric Vortex si hay enemigos en rango
