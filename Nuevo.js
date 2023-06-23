c66c30d345e278c04123a17fff153cad4f90fa23
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-22 23:40:04 -0500

    Update Nuevo.js

--- Nuevo.js
+++ Nuevo.js
@@ -294,21 +294,7 @@
 				if (Engine.OnceAt(0.2)) {
 
 					if (comboTarget && comboTarget.IsExist()) {
-						
-						let [linken, mirror] = [comboTarget.GetItem('item_sphere', true), comboTarget.GetItem('item_mirror_shield', false)];
-                        if (linken && linken.CanCast() || mirror && mirror.CanCast()) {
-                            let linkenBrokItems = menu_LinkensItems.GetValue();
-                            for (let brokObj of linkenBrokItems) {
-                                let vi = localHero.GetItem(brokObj, false);
-                                if (vi) {
-                                    if (vi.IsExist() && CustomCanCast(vi)) {
-                                        vi.CastTarget(comboTarget);
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-						
+												
 						const localHeroPosition = localHero.GetAbsOrigin();
 						const attackRange = localHero.GetAttackRange();
 						const enemyHeroPosition = comboTarget.GetAbsOrigin();
@@ -370,6 +356,20 @@
 								}
 							}
 						}
+						
+						let [linken, mirror] = [comboTarget.GetItem('item_sphere', true), comboTarget.GetItem('item_mirror_shield', false)];
+                        if (linken && linken.CanCast() || mirror && mirror.CanCast()) {
+                            let linkenBrokItems = menu_LinkensItems.GetValue();
+                            for (let brokObj of linkenBrokItems) {
+                                let vi = localHero.GetItem(brokObj, false);
+                                if (vi) {
+                                    if (vi.IsExist() && CustomCanCast(vi)) {
+                                        vi.CastTarget(comboTarget);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
 						
 
 						let CastVortex = false;
