178f234c82554be9281c85dec82052bd6a50e28e
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-24 16:38:56 -0500

    Update FurionAbuse.js

--- FurionAbuse.js
+++ FurionAbuse.js
@@ -31,60 +31,65 @@
 
 
 	// Definición de la función OnUpdate
-	AutoSaverAlchemist.OnUpdate = () => {
-	  if (localHero && isUiEnabled.GetValue()) {
-		if (localHero.GetUnitName() !== "npc_dota_hero_alchemist")
-		  return;
-		console.log("Hola mundo");
-		// Obtener la posición del mouse
-		const mousePos = Input.GetWorldCursorPos();
-		const heroPos = localHero.GetAbsOrigin();
-		const distance = heroPos.Distance(mousePos);
+AutoSaverAlchemist.OnUpdate = () => {
+  if (localHero && isUiEnabled.GetValue()) {
+    if (localHero.GetUnitName() !== "npc_dota_hero_alchemist")
+      return;
+    console.log("Hola mundo");
+    // Obtener la posición del mouse
+    const mousePos = Input.GetWorldCursorPos();
+    const heroPos = localHero.GetAbsOrigin();
+    const distance = heroPos.Distance(mousePos);
 
-		// Calcular el costo de maná por distancia recorrida
-		const manaCost = Math.floor(25 + (0.075 * localHero.GetMaxMana()) + (0.01 * localHero.GetMaxMana() * Math.floor(distance / 100)));
+    // Calcular el costo de maná por distancia recorrida
+    const manaCost = Math.floor(25 + (0.075 * localHero.GetMaxMana()) + (0.01 * localHero.GetMaxMana() * Math.floor(distance / 100)));
 
-		// Calcular el daño por distancia recorrida
-		//const damage = Math.floor((8 + (4 * localHero.GetAbilityByIndex(5).GetLevel())) * Math.floor(distance / 100));
-		const damage = 100;
-		const font = Renderer.LoadFont("Tahoma", 10, Enum.FontWeight.EXTRABOLD);
+    // Calcular el daño por distancia recorrida
+    //const damage = Math.floor((8 + (4 * localHero.GetAbilityByIndex(5).GetLevel())) * Math.floor(distance / 100));
+    const damage = 100;
+    const font = Renderer.LoadFont("Tahoma", 10, Enum.FontWeight.EXTRABOLD);
 
-		// Dibujar el panel
-		Renderer.SetDrawColor(0, 0, 0, 150);  
-		Renderer.DrawFilledRect(panelX, panelY, panelWidth, panelHeight, 4);
+    // Dibujar el panel
+    Renderer.SetDrawColor(0, 0, 0, 150);  
+    Renderer.DrawFilledRect(panelX, panelY, panelWidth, panelHeight, 4);
 
-		Renderer.SetDrawColor(0, 0, 0, 255);  
-		Renderer.DrawFilledRect(panelX + 5, panelY + 3, panelWidth - 11, 9, 4);                    
+    Renderer.SetDrawColor(0, 0, 0, 255);  
+    Renderer.DrawFilledRect(panelX + 5, panelY + 3, panelWidth - 11, 9, 4);                    
 
-		Renderer.SetDrawColor(53, 153, 220, 255);  
-		Renderer.DrawFilledRect(panelX + 6, panelY + 4, panelWidth - 12, 8, 4);
+    Renderer.SetDrawColor(53, 153, 220, 255);  
+    Renderer.DrawFilledRect(panelX + 6, panelY + 4, panelWidth - 12, 8, 4);
 
-		Renderer.SetDrawColor(255, 255, 255, 255);
-		Renderer.DrawText(font, panelX + 25, panelY + 4, "" + manaCost, 1);
-		Renderer.DrawText(font, panelX + 30, panelY + 16, "" + damage, 1);
+    Renderer.SetDrawColor(255, 255, 255, 255);
+    Renderer.DrawText(font, panelX + 25, panelY + 4, "" + manaCost, 1);
+    Renderer.DrawText(font, panelX + 30, panelY + 16, "" + damage, 1);
 
-		Renderer.SetDrawColor(255, 255, 255, 255);
-		let imageHandle = Renderer.LoadImage("panorama/images/hud/icon_kill_png.vtex_c");
-		Renderer.DrawImage(imageHandle, panelX + 7, panelY + 14, 12, 12);
+    Renderer.SetDrawColor(255, 255, 255, 255);
+    let imageHandle = Renderer.LoadImage("panorama/images/hud/icon_kill_png.vtex_c");
+    Renderer.DrawImage(imageHandle, panelX + 7, panelY + 14, 12, 12);
 
-		// Agregar evento de clic al panel
-		if (Input.IsKeyDown(Enum.ButtonCode.KEY_LCONTROL) && Input.IsCursorInRect(panelX, panelY, panelWidth, panelHeight)) {
-		  if (Input.IsKeyDown(Enum.ButtonCode.MOUSE_LEFT)) {
-			if (!isDragging) {
-			  isDragging = true;
-			  dragOffsetX = mousePos[0] - panelX;
-			  dragOffsetY = mousePos[1] - panelY;
-			} else {
-			  panelX = mousePos[0] - dragOffsetX;
-			  panelY = mousePos[1] - dragOffsetY;
-			}
-		  } else {
-			isDragging = false;
-		  }
-		}
-	  }
-	};
+    // Agregar evento de clic al panel
+    if (Input.IsKeyDown(Enum.ButtonCode.KEY_LCONTROL) && Input.IsCursorInRect(panelX, panelY, panelWidth, panelHeight)) {
+      if (!isDragging && Input.IsKeyDown(Enum.ButtonCode.MOUSE_LEFT)) {
+        isDragging = true;
+        dragOffsetX = mousePos[0] - panelX;
+        dragOffsetY = mousePos[1] - panelY;
+      }
+    } else {
+      isDragging = false;
+      if (Input.IsKeyDown(Enum.ButtonCode.MOUSE_LEFT)) {
+        // Si el panel no está siendo arrastrado, el evento de clic lo ocultará
+        Renderer.SetDrawColor(0, 0, 0, 0);
+        Renderer.DrawFilledRect(panelX, panelY, panelWidth, panelHeight, 4);
+      }
+    }
 
+    // Actualizar la posición del panel si está siendo arrastrado
+    if (isDragging) {
+      panelX = mousePos[0] - dragOffsetX;
+      panelY = mousePos[1] - dragOffsetY;
+    }
+  }
+};
 	// Definición de la OnScriptLoad
 	AutoSaverAlchemist.OnScriptLoad = AutoSaverAlchemist.OnGameStart = () => {
 		localHero = EntitySystem.GetLocalHero();
