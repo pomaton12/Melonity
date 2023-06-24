757db6086d76e7542da36633d54fe344a46b064b
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-24 13:46:40 -0500

    Update FurionAbuse.js

--- FurionAbuse.js
+++ FurionAbuse.js
@@ -768,12 +768,15 @@
 				const [x, y, visible] = Renderer.WorldToScreen(mousePos);
 				if (visible) {
 					Renderer.SetDrawColor(0, 0, 0, 150);  
-					Renderer.DrawFilledRect(x, y, 65, 30, 5);
+					Renderer.DrawFilledRect(x, y, 65, 28, 5);
 					
-					Renderer.SetDrawColor(200, 200, 200, 255);
+					Renderer.SetDrawColor(255, 255, 255, 255);
 					Renderer.DrawText(font, x, y, "Daño: " + x, 1);
 					Renderer.DrawText(font, x, y + 10, "Costo de Maná: " + y, 1);
 					
+					Renderer.SetDrawColor(255, 255, 255, 255).
+					let img = Renderer.LoadImage("panorama/images/hud/icon_kill_png.vtex_c");
+					Renderer.DrawImage(img, x, y);
 					
 
 				}
