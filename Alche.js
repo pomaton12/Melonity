12b3e89f5a7d01126069644df0a2eca0996ac7be
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-24 16:26:59 -0500

    Update Alche.js

--- Alche.js
+++ Alche.js
@@ -39,43 +39,23 @@
         if (localHero && isUiEnabled) {
             if (localHero.GetUnitName() !== "npc_dota_hero_alchemist")
                 return;
-			console.log("Hola mundo");
-			// Obtener la posición del mouse
-			const mousePos = Input.GetWorldCursorPos();
-			const heroPos = localHero.GetAbsOrigin();
-			const distance = heroPos.Distance(mousePos);
-
-			// Calcular el costo de maná por distancia recorrida
-			const manaCost =  Math.floor(25 + (0.075 * localHero.GetMaxMana()) + (0.01 * localHero.GetMaxMana() * Math.floor(distance / 100)));
-
-			// Calcular el daño por distancia recorrida
-			//const damage = Math.floor((8 + (4 * localHero.GetAbilityByIndex(5).GetLevel())) * Math.floor(distance / 100));
-			const damage =100;
-			const font = Renderer.LoadFont("Tahoma", 10, Enum.FontWeight.EXTRABOLD);
-			// Mostrar los resultados en la pantalla
-
-
-			//Panel a mover si se presionar CTRL + click  en el panel   y arrastrar a cualquier parte y soltar en su nueva posicion
-			let Ledsize = Renderer.GetScreenSize();
-			let x = Ledsize[0]/2;
-			let y = Ledsize[1]/2;
-
-			Renderer.SetDrawColor(0, 0, 0, 150);  
-			Renderer.DrawFilledRect(x, y, 65, 28, 4);
-
-			Renderer.SetDrawColor(0, 0, 0, 255);  
-			Renderer.DrawFilledRect(x+5, y+3, 65-11, 9, 4);                    
-
-			Renderer.SetDrawColor(53, 153, 220,255);  
-			Renderer.DrawFilledRect(x+6, y+4, 65-12, 8, 4);
-
-			Renderer.SetDrawColor(255, 255, 255, 255);
-			Renderer.DrawText(font,+25, y+4, ""+manaCost, 1);
-			Renderer.DrawText(font, x+30, y + 16,""+damage, 1);
-
-			Renderer.SetDrawColor(255, 255, 255, 255);
-			let imageHandle = Renderer.LoadImage("panorama/images/hud/icon_kill_png.vtex_c");
-			Renderer.DrawImage(imageHandle, x+7, y+14, 12, 12);
+            const modifiers = localHero.GetModifiers();
+            for (let modifier of modifiers) {
+                if (modifier.GetName() === 'modifier_alchemist_unstable_concoction') {
+                    const remainingTime = modifier.GetRemainingTime();
+                    if (remainingTime <= -6.45) {
+                        const manta = localHero.GetItem('item_manta', true);
+                        const bkb = localHero.GetItem('item_black_king_bar', true);
+                        if (itemSelection[1] && manta && manta.GetCooldown() === 0) {
+                            manta.CastNoTarget();
+                            break;
+                        } else if (itemSelection[0] && (!manta || manta.GetCooldown() > 0) && bkb && bkb.GetCooldown() === 0) {
+                            bkb.CastNoTarget();
+                            break;
+                        }
+                    }
+                }
+            }
         }
     };
 
