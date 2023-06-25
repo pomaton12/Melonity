c861a0b6a0f081688f14b87bc92ac9b4f0ece2b1
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-24 20:20:38 -0500

    Update Nuevo.js

--- Nuevo.js
+++ Nuevo.js
@@ -699,7 +699,7 @@
 
 									if (comboTarget.IsAttacking() || EnemiPrevention.length >= 3) {
 										// Calcula una nueva posición detrás del enemigo	
-										if (GameRules.GetGameTime() - lastUltimateTime >= 2.5) {
+										if (GameRules.GetGameTime() - lastUltimateTime >= 2) {
 											let IdealPosition = localHeroPosition.add(Idealdirection.mul(new Vector(DistanceCastUI, DistanceCastUI, 0)));
 											myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_CAST_POSITION,null,IdealPosition,Ultimate, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero);
 											lastUltimateTime = GameRules.GetGameTime();
