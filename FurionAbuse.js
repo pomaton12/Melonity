bdda67e96c876f3c29ce4d4905003038a6753c75
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-26 18:37:02 -0500

    Update FurionAbuse.js

--- FurionAbuse.js
+++ FurionAbuse.js
@@ -93,6 +93,12 @@
 									Renderer.DrawText(font1, Math.ceil(xpos)+2, Math.ceil(ypos)+12, ""+abilLvl);
 									
 									//console.log(ability.GetAbilityCharges());
+									const cooldownRemaining = ability.GetCooldownTimeRemaining();
+									if (cooldownRemaining === 0) {
+										const charges = ability.GetCurrentCharges();
+										console.log(charges);
+									}
+																		
 									
 								} else {
 									Renderer.SetDrawColor(255,255, 255, 255);
