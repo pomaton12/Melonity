115a5a7e07ae8e10f6bd18557fdb5110c647308e
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-26 11:28:03 -0500

    Update FurionAbuse.js

--- FurionAbuse.js
+++ FurionAbuse.js
@@ -26,7 +26,7 @@
         if (localHero && isUiEnabled.GetValue()) {
 			
 		let panel = Renderer.CreatePanel("EnemyHeroPanel", null, {width = 200, height = 200});
-		panel.SetText("Héroes enemigos y sus habilidades");
+		//panel.SetText("Héroes enemigos y sus habilidades");
 					
         }
     };		
