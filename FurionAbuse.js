b8810c45a6e9a2a0707960a6a6889f756e33cba4
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-26 11:19:10 -0500

    Update FurionAbuse.js

--- FurionAbuse.js
+++ FurionAbuse.js
@@ -21,27 +21,12 @@
 	let isUiEnabled = Menu.AddToggle(path_, 'Skill Alert', true);
 
 
-	var modifiers = [
-		["modifier_invoker_sun_strike", 1.7],
-		["modifier_kunkka_torrent_thinker", 1.6],
-		["modifier_lina_light_strike_array", 0.5],
-		["modifier_leshrac_split_earth_thinker", 0.35],
-		["modifier_spirit_breaker_charge_of_darkness_vision", 1.5],
-		["modifier_tusk_snowball_visible", 1.5]
-	]
-
-	var z = []
-
 	// Definición de la función OnUpdate
 	CreatePanel.OnUpdate = () => {
         if (localHero && isUiEnabled.GetValue()) {
 			
-			//Game.ScriptLogMsg('Script enabled: SkillAlert', '#00ff00');
-			
-			var Temp = CreatePanel( "Panel", Game.GetMainHUD(), "SkillAlert" );
-			Temp.SetPanelEvent( 'onactivate', SkillAlertChkBox );
-			Temp.BLoadLayoutFromString( '<root><styles><include src="s2r://panorama/styles/dotastyles.vcss_c" /><include src="s2r://panorama/styles/magadan.vcss_c" /></styles><Panel><ToggleButton class="CheckBox" id="SkillAlert" text="SkillAlert"/></Panel></root>', false, false);  
-			var SkillAlert = $.GetContextPanel().FindChildTraverse( 'SkillAlert' ).Children()[0];
+		let panel = Renderer.CreatePanel("EnemyHeroPanel", null, {width = 200, height = 200});
+		panel.SetText("Héroes enemigos y sus habilidades");
 					
         }
     };		
