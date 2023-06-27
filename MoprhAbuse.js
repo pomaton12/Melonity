a06c055ba9484d24adb10b8ebaf379b6ab867971
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-27 14:31:46 -0500

    Create MoprhAbuse.js

---
+++ MoprhAbuse.js
@@ -0,0 +1,154 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/MorphlingUltiAbuse.ts":
+/*!**********************************!*\
+  !*** ./src/MorphlingUltiAbuse.ts ***!
+  \**********************************/
+/***/ (() => {
+
+	// Definición del objeto MorphlingUltiAbuse
+	const MorphlingUltiAbuse = {};
+
+	// Declaración de la variable localHero
+	let localHero;
+	let [sizescrx,sizescry] = Renderer.GetScreenSize();
+
+	let xpos = sizescrx/2;
+	let ypos = sizescry/2;
+	let enemyList = [];
+	let cooldowns = [];
+
+	// Definición del array path_
+	const path_ = ["Custom Scripts","Morphling"];
+
+	// Creación del toggle isUiEnabled
+	let isUiEnabled = Menu.AddToggle(path_, 'Skill Alert', true);
+
+	// Definición de la función OnUpdate
+	MorphlingUltiAbuse.OnUpdate = () => {
+        if (localHero && isUiEnabled.GetValue()) {			
+			if (localHero.GetUnitName() !== "npc_dota_hero_storm_spirit") {
+				return;
+			}
+			
+			let sizeamountx = 120;
+			let visibility = 255;
+			let sizeBarx = sizeamountx / 3 * 0.75;
+			let sizeBary = sizeBarx * 0.7; 
+			
+			let PANEL_WIDTH = sizeBarx*6;
+			let PANEL_HEIGHT = sizeBary*5;
+
+						
+			//let sizeBary = sizeBarx; 
+			let font = Renderer.LoadFont("Tahoma", 10, Enum.FontWeight.EXTRABOLD);
+			let font1 = Renderer.LoadFont("Tahoma", 8, Enum.FontWeight.EXTRABOLD);
+			
+			let enemyList = [];					
+			let heroes = EntitySystem.GetHeroesList();
+			if (heroes) {
+				for (let hero of heroes) {
+					if (hero && !hero.IsIllusion() && !hero.IsMeepoClone() && hero.IsHero()  && !hero.IsSameTeam(localHero)) {
+						enemyList.push(hero);
+					}
+				}
+			}
+			
+			
+			for (let hero of enemyList) {
+				Renderer.SetDrawColor(255, 255, 255, visibility);
+				if (hero) {
+					let imageHandle;
+					let heroNAME = hero.GetUnitName();
+					let IdHERO = hero.GetPlayerID();
+				
+					imageHandle = Renderer.LoadImage("panorama/images/heroes/" + heroNAME + "_png.vtex_c");
+
+					if (imageHandle) {
+						let herolvl = hero.GetCurrentLevel();
+						Renderer.DrawImage(imageHandle, Math.ceil(xpos), Math.ceil(ypos), Math.ceil(sizeBarx), Math.ceil(sizeBary));
+						Renderer.SetDrawColor(120, 0, 255, 255);
+						Renderer.DrawOutlineRect(Math.ceil(xpos), Math.ceil(ypos), Math.ceil(sizeBarx), Math.ceil(sizeBary));
+						Renderer.SetDrawColor(255, 255, 0, 255);
+						Renderer.DrawText(font, Math.ceil(xpos)+2, Math.ceil(ypos)+12, ""+herolvl);
+					}
+					xpos = xpos + sizeBarx+5;
+					for (let i = 0; i < 6; i++) {
+						let ability = hero.GetAbilityByIndex(i);
+						//console.log(ability);
+						if (ability) {
+							let abilityImageHandle;
+							let AbilNAME = ability.GetName();
+							abilityImageHandle = Renderer.LoadImage("panorama/images/spellicons/" + AbilNAME + "_png.vtex_c");
+							
+							let key = IdHERO+ heroNAME + AbilNAME;
+
+							// Si la habilidad no está en la lista, agregarla
+							if (!cooldowns[key]) {
+								cooldowns[key] = [IdHERO, heroNAME, AbilNAME, 0, 0];
+							}
+
+							// Actualizar la posición de la habilidad en la lista
+							cooldowns[key][3] = xpos;
+							cooldowns[key][4] = ypos;
+
+							if (ability.GetLevel() >= 1) {
+								
+								if (ability.IsReady() && ability.IsActivated()) {
+									Renderer.SetDrawColor(255, 255, 255, visibility);
+									Renderer.DrawImage(abilityImageHandle, Math.ceil(xpos), Math.ceil(ypos), Math.ceil(sizeBarx), Math.ceil(sizeBary));
+									Renderer.SetDrawColor(0, 255, 0, visibility);
+									Renderer.DrawOutlineRect(Math.ceil(xpos), Math.ceil(ypos), Math.ceil(sizeBarx), Math.ceil(sizeBary));
+								}
+							} else{
+								Renderer.SetDrawColor(255,255, 255, 255);
+								Renderer.DrawImage(abilityImageHandle, Math.ceil(xpos), Math.ceil(ypos), Math.ceil(sizeBarx), Math.ceil(sizeBary));
+								Renderer.SetDrawColor(0,0, 0, 150);
+								Renderer.DrawFilledRect( Math.ceil(xpos), Math.ceil(ypos), Math.ceil(sizeBarx), Math.ceil(sizeBary));
+							}
+							
+							xpos = xpos + sizeBarx;
+						}
+					}
+					ypos = ypos + sizeBary;
+					xpos = sizescrx/2;
+				}
+			}
+			
+			// Detectar si se mantiene presionada la tecla Control
+			//if (Input.IsKeyDown(Enum.ButtonCode.KEY_LCONTROL) ) {
+				//if (Input.IsKeyDown(Enum.ButtonCode.MOUSE_LEFT) && Input.IsCursorInRect(xpos, ypos, PANEL_WIDTH, PANEL_HEIGHT)) {
+
+				//}
+			//} 
+        }
+    };		
+	
+	
+  // Definición de la función OnScriptLoad
+  MorphlingUltiAbuse.OnScriptLoad = MorphlingUltiAbuse.OnGameStart = () => {
+      localHero = EntitySystem.GetLocalHero();
+  };
+
+  // Definición de la función OnGameEnd
+  MorphlingUltiAbuse.OnGameEnd = () => {
+      localHero = null;
+  };
+
+  // Registro del script
+  RegisterScript(MorphlingUltiAbuse);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/MorphlingUltiAbuse.ts"]();
+/******/ 	
+/******/ })()
+;
