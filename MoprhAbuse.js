4570326f0fc13cf44daa28fbaa4ffa3291eaa375
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-29 18:41:10 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -1,7 +1,7 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/MorphlingUltiAbuse.ts":
+/***/ "./src/MorphlingUltiAbuse.ts": 
 /*!**********************************!*\
   !*** ./src/MorphlingUltiAbuse.ts ***!
   \**********************************/
