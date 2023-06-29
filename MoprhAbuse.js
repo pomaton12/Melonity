c5992e825a1570efbf8a3863ce635e1e17274237
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-06-29 11:01:58 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -1,4 +1,4 @@
-/******/ (() => { // webpackBootstrap 
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/MorphlingUltiAbuse.ts":
@@ -360,22 +360,22 @@
 								
 								const behavior = AbilHybrid.GetBehavior();
 								console.log(Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_UNIT_TARGET);
-								if (behavior === Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_NO_TARGET) {
+								if (behavior & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_NO_TARGET) {
 									// La habilidad es activable.
 									console.log("La habilidad es activable.");
 								
-								} else if (behavior === Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_UNIT_TARGET) {
+								} else if (behavior & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_UNIT_TARGET) {
 									const targetTeam = AbilHybrid.GetTargetTeam();
-									if (targetTeam === Enum.TargetTeam.DOTA_UNIT_TARGET_TEAM_FRIENDLY) {
+									if (targetTeam & Enum.TargetTeam.DOTA_UNIT_TARGET_TEAM_FRIENDLY) {
 										// La habilidad es de tipo con objetivo y se puede usar en unidades aliadas, incluyéndose a uno mismo.
 										console.log("Cast Aliados y yo");
 										AbilHybrid.CastTarget(localHero);
 										
-									} else if (targetTeam === Enum.TargetTeam.DOTA_UNIT_TARGET_TEAM_ENEMY) {
+									} else if (targetTeam & Enum.TargetTeam.DOTA_UNIT_TARGET_TEAM_ENEMY) {
 										// La habilidad es de tipo con objetivo y solo se puede usar en unidades enemigas.
 										console.log("Cast Enemigos");
 									}
-								} else if (behavior === Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_POINT) {
+								} else if (behavior & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_POINT) {
 									// La habilidad es de tipo con objetivo y requiere una ubicación en el mapa.
 									console.log("Casteo en una posicion");
 									
