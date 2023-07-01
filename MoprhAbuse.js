5a2806a26822a41a77c385f5f4852f762cbe2b40
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-01 08:21:21 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -422,7 +422,7 @@
 													const behavior = AbilHybrid.GetBehavior();
 													if ((behavior & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_NO_TARGET) && !(behavior & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_TOGGLE)) {
 														// La habilidad es activable.
-														console.log(AbilHybrid.GetSecondaryCharges());
+														console.log(AbilHybrid.GetCooldown());
 														let aoe_radius = AbilHybrid.GetLevelSpecialValueFor("radius");
 														let AttackRangeBasic = localHero.GetAttackRange();
 														let AttackRangeBuff = localHero.GetAttackRangeBonus();
