909bdb79691bbd402ccb3d28780ba9b861ae2889
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-03 11:28:23 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -201,7 +201,7 @@
             if (comboTarget) {
                 if (!particle) {
                     //particle = Particle.Create('particles/ui_mouseactions/range_finder_tower_aoe.vpcf', Enum.ParticleAttachment.PATTACH_INVALID, comboTarget);
-					particle = Particle.Create("particles/creatures/candy_well/bucket_soldier_leash.vpcf", Enum.ParticleAttachment.PATTACH_INVALID, 0);
+					particle = Particle.Create("particles/units/heroes/hero_invoker/invoker_sun_strike.vpcf", Enum.ParticleAttachment.PATTACH_INVALID, 0);
 					particle.SetControl(0, comboTarget.GetAbsOrigin());
 					//particle.SetControl(0, Vector(209,21,255));
 					particle.SetControl(1, Vector(500,0,0));
