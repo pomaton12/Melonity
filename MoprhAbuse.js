7fa21af51347c46649f57f558ba8375157e07e3b
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-03 11:47:15 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -201,12 +201,12 @@
             if (comboTarget) {
                 if (!particle) {
                     //particle = Particle.Create('particles/ui_mouseactions/range_finder_tower_aoe.vpcf', Enum.ParticleAttachment.PATTACH_INVALID, comboTarget);
-					particle = Particle.Create("particles/items_fx/aura_shivas.vpcf", Enum.ParticleAttachment.PATTACH_ABSORIGIN, comboTarget);
-					particle.SetControl(7, comboTarget.GetAbsOrigin());
+					particle = Particle.Create("particles/units/heroes/hero_spirit_breaker/spirit_breaker_charge_target_mark.vpcf", Enum.ParticleAttachment.PATTACH_OVERHEAD_FOLLOW, comboTarget);
+					//particle.SetControl(7, comboTarget.GetAbsOrigin());
 					//particle.SetControl(0, Vector(209,21,255));
-					particle.SetControl(1, Vector(500,0,0));
+					//particle.SetControl(1, Vector(500,0,0));
 					//particle.SetControl(2, EntitySystem.GetLocalHero().GetAbsOrigin());
-					particle.SetControl(6, new Vector(1, 0, 0));
+					//particle.SetControl(6, new Vector(1, 0, 0));
 					
                 }
                 else {
