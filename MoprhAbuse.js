1b51dc5d01414deaa7400bf54ba8fbceb8f79df5
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-03 12:56:36 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -916,9 +916,9 @@
 				let enemiesMorRange = localHero.GetHeroesInRadius(700, Enum.TeamType.TEAM_ENEMY);
 	
 				if (manta && CustomCanCast(manta) && !invimod) { 
-					
+					console.log(MyHeroSilenced);
 					if (MyHeroSilenced && enemiesMorRange.length > 0) {
-						console.log(MyHeroSilenced);
+						
 						manta.CastNoTarget();
 					}
 				}
