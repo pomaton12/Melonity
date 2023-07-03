437d5e27a3b422a05735b99d45fe7f8e578e06f0
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-03 09:40:53 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -202,9 +202,10 @@
                 if (!particle) {
                     //particle = Particle.Create('particles/ui_mouseactions/range_finder_tower_aoe.vpcf', Enum.ParticleAttachment.PATTACH_INVALID, comboTarget);
 				particle = Particle.Create("particles/econ/items/spectre/spectre_arcana/spectre_arcana_minigame_death_target.vpcf", Enum.ParticleAttachment.PATTACH_ABSORIGIN_FOLLOW, comboTarget);
-				particle.SetControl(0, comboTarget.GetAbsOrigin());
+				//particle.SetControl(0, comboTarget.GetAbsOrigin());
 				//particle.SetControl(1, Vector(500,0,0));
 				particle.SetControl(6, new Vector(1, 0, 0));
+				particle.SetControl(7, comboTarget.GetAbsOrigin());
                 }
                 else {
                     //particle.SetControl(2, EntitySystem.GetLocalHero().GetAbsOrigin());
