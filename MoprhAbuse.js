527d7e0f8104f6ecafc56c08ee8e9bd2c3f751c7
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-03 11:48:50 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -201,7 +201,7 @@
             if (comboTarget) {
                 if (!particle) {
                     //particle = Particle.Create('particles/ui_mouseactions/range_finder_tower_aoe.vpcf', Enum.ParticleAttachment.PATTACH_INVALID, comboTarget);
-					particle = Particle.Create("particles/units/heroes/hero_spirit_breaker/spirit_breaker_charge_target_mark.vpcf", Enum.ParticleAttachment.PATTACH_OVERHEAD_FOLLOW, comboTarget);
+					particle = Particle.Create("particles/econ/items/windrunner/windranger_arcana/windranger_arcana_death.vpcf", Enum.ParticleAttachment.PATTACH_OVERHEAD_FOLLOW, comboTarget);
 					//particle.SetControl(7, comboTarget.GetAbsOrigin());
 					//particle.SetControl(0, Vector(209,21,255));
 					//particle.SetControl(1, Vector(500,0,0));
