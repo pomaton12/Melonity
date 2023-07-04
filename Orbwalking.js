f7e45b66db4970c7824c989269672035ad34ee4c
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-04 11:59:12 -0500

    Update Orbwalking.js

--- Orbwalking.js
+++ Orbwalking.js
@@ -26,7 +26,9 @@
 
 	let KeyBindOrbwalk = Menu.AddKeyBind(path_, 'Key of OrbWalk', Enum.ButtonCode.KEY_NONE);
 
-	let isUiEnabled2 = Menu.AddToggle(path_, 'Kill Safe Pos', false);
+	let isUiEnabled2 = Menu.AddToggle(path_, 'Kill Safe Pos (No recomend)', false);
+	
+	let isUiEnabled3 = Menu.AddToggle(path_, 'Auto Orbwalk on right click hero enemy', true);
 	
 	let DisplayMode = Menu.AddComboBox(path_, 'Display', ['To Enemy', 'Mouse position'],0)
 	.OnChange(state =>{	DisplayMode = state.newValue;})
@@ -76,11 +78,14 @@
 	// Definición de la función startMouseBoost
 	HitRunHeros.OnPrepareUnitOrders = (event) => {
 		if (localHero && isUiEnabled1.GetValue()) {
-			if (exOrders.includes(event.order)) {
-				let target = event.target;
+			if (isUiEnabled3.GetValue()) {
+				if (exOrders.includes(event.order)) {
+					let EnemiHero = event.target;
 
-				if (target && target.IsHero() && target.IsAlive() && !target.IsDormant() && target.IsOpposingTeam(myHero.GetTeamNumber())) {
-					console.log("nombre_del_hero ", target.GetPlayerID()); // Reemplaza "nombre_de_la_habilidad" con el nombre de la habilidad que quieres lanzar
+					if (EnemiHero && EnemiHero.IsHero() && EnemiHero.IsAlive() && !EnemiHero.IsDormant() && EnemiHero.IsOpposingTeam(localHero.GetTeamNumber())) {
+						console.log("nombre_del_hero ", EnemiHero.GetPlayerID()); // Reemplaza "nombre_de_la_habilidad" con el nombre de la habilidad que quieres lanzar
+						HitRunOrbFunction(enemiHero);
+					}
 				}
 			}
 		}
@@ -113,72 +118,77 @@
 					const distB = b.GetAbsOrigin().Distance(localHero.GetAbsOrigin());
 					return distA - distB;
 				});
-				let target = null;
+				let enemiHero = null;
 				for (const enemy of enemies) {
 					const dist = enemy.GetAbsOrigin().Distance(mousePos);
-					if (dist <= 100 || target == null) {
-						target = enemy;
+					if (dist <= 150 || enemiHero == null) {
+						enemiHero = enemy;
 					}
 				}
-				if (Engine.OnceAt(0.2)) {
-					if (target != null) {
-						const localHeroPosition = localHero.GetAbsOrigin();
-						const EnemyHero = target;
-						const RangeBasic = localHero.GetAttackRange();
-						const RangeBuff = localHero.GetAttackRangeBonus();
-						const attackRange = RangeBasic + RangeBuff;
-						const enemyHeroPosition = EnemyHero.GetAbsOrigin();
-						const dist = localHeroPosition.Distance(enemyHeroPosition) - 50;
-						const attackSpeed = localHero.GetAttacksPerSecond();
-						const attackTime = 1 / attackSpeed;
-						if (DisplayMode === 0 ) {							
-							let newRange = attackRange * (SafeDistanceUIval / 100);
-							if (5 >= newRange) {
-								newRange = 5;
-							}
-							
-							
-							
-							if (dist >= newRange) {
-								if (!isUiEnabled2.GetValue()) {
-									if (newRange) {
-										pos1 = localHeroPosition.add(new Vector(dist - newRange).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
-										if (parseInt(newRange) == parseInt(dist)) {
-											myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
-										} else {
-											myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
-											
-											if (Engine.OnceAt(attackTime)) {
-												
-												myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, pos1, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
-											}
-										}
-									}
-								}
-							} else {
-								if (isUiEnabled2.GetValue()) {
-									pos1 = localHeroPosition.add(new Vector(dist - newRange - 100).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
+
+				HitRunOrbFunction(enemiHero);
+			}
+		}
+	};
+	
+	function HitRunOrbFunction(tarjet)
+		if (Engine.OnceAt(0.2)) {
+			if (target != null) {
+				const localHeroPosition = localHero.GetAbsOrigin();
+				const EnemyHero = target;
+				const RangeBasic = localHero.GetAttackRange();
+				const RangeBuff = localHero.GetAttackRangeBonus();
+				const attackRange = RangeBasic + RangeBuff;
+				const enemyHeroPosition = EnemyHero.GetAbsOrigin();
+				const dist = localHeroPosition.Distance(enemyHeroPosition) - 50;
+				const attackSpeed = localHero.GetAttacksPerSecond();
+				const attackTime = 1 / attackSpeed;
+				if (DisplayMode === 0 ) {							
+					let newRange = attackRange * (SafeDistanceUIval / 100);
+					if (5 >= newRange) {
+						newRange = 5;
+					}
+					
+					
+					
+					if (dist >= newRange) {
+						if (!isUiEnabled2.GetValue()) {
+							if (newRange) {
+								pos1 = localHeroPosition.add(new Vector(dist - newRange).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
+								if (parseInt(newRange) == parseInt(dist)) {
+									myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
+								} else {
+									myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, EnemyHero, enemyHeroPosition, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
+									
 									if (Engine.OnceAt(attackTime)) {
+										
 										myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, pos1, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
 									}
 								}
 							}
-						} else if (DisplayMode === 1) {
-							if (attackTime > 0.6) {
-								timepusepos = attackTime;
-							} else {
-								timepusepos = 0.6;
-							}
-							const mouse = Input.GetWorldCursorPos();
-							if (Engine.OnceAt(timepusepos)) {
-								localHero.MoveTo(mousePos);
+						}
+					} else {
+						if (isUiEnabled2.GetValue()) {
+							pos1 = localHeroPosition.add(new Vector(dist - newRange - 100).Rotated(GetAngleToPos(localHeroPosition, enemyHeroPosition)));
+							if (Engine.OnceAt(attackTime)) {
+								myPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, null, pos1, null, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, localHero, false, true);
 							}
 						}
+					}
+				} else if (DisplayMode === 1) {
+					if (attackTime > 0.6) {
+						timepusepos = attackTime;
+					} else {
+						timepusepos = 0.6;
+					}
+					const mouse = Input.GetWorldCursorPos();
+					if (Engine.OnceAt(timepusepos)) {
+						localHero.MoveTo(mousePos);
 					}
 				}
 			}
 		}
-	};
+	}
 
 	HitRunHeros.OnScriptLoad = HitRunHeros.OnGameStart = () => {
 	  localHero = EntitySystem.GetLocalHero();
