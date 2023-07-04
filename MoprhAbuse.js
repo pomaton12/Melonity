40645a2cdd8a601dc348d09a3edf645efd56ad43
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-03 19:30:33 -0500

    Update MoprhAbuse.js

--- MoprhAbuse.js
+++ MoprhAbuse.js
@@ -548,15 +548,17 @@
 																		const castpointTimee = AbilHybrid.GetCastPoint();
 																		const delay = travel_time + castpointTimee;
 																		const BestPost = GetPredictedPosition(comboTarget, delay);
-																		const BestPost1 = BestPost.add(new Vector(100, 100, 0));
+																		//const BestPost1 = BestPost.add(new Vector(100, 100, 0));
 																		//const BestPost = GetBestPost(localHero, comboTarget);
 																		
 																		AbilHybritList[keyAbil][2] = GameRules.GetGameTime();
 																		AbilHybritList[keyAbil][3] = AbilHybrid.GetCooldown();
 																		
-																		myPlayer.PrepareUnitOrders(30, null, BestPost1, AbilHybrid, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY, localHero);
-																		AbilHybrid.CastPosition(IdealPosition);
-																		setTimeout(function() {}, 300);
+																		
+																		setTimeout(function() {
+																		myPlayer.PrepareUnitOrders(30, null, IdealPosition, AbilHybrid, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY, localHero);
+																		AbilHybrid.CastPosition(BestPost);	
+																		}, 300);
 
 																	}
 																	
