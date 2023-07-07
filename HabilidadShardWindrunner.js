02f362d6d507e47feba23495ed73b5ac5ddcf6e0
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-07-06 19:02:03 -0500

    Update HabilidadShardWindrunner.js

--- HabilidadShardWindrunner.js
+++ HabilidadShardWindrunner.js
@@ -400,7 +400,7 @@
 							let itemblink = localHero.GetItem('item_blink', true) || localHero.GetItem('item_overwhelming_blink', true) || localHero.GetItem('item_arcane_blink', true) || localHero.GetItem('item_swift_blink', true);
 							if (itemblink && CustomCanCast(itemblink) && !MyModSilverEdge) { 
 								let  castRange = itemblink.GetLevelSpecialValueFor("abilitycastrange");
-								console.log(Trees.Count());
+								console.log(Tree.Count());
 								let castRangeBonus = localHero.GetCastRangeBonus();
 								let castRangeTotal =  castRange + castRangeBonus;
 								
