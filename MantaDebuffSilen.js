dacf3012a30a87a2d8c149f2d7be9325f4f6683d
Ethereal <129473476+pomaton12@users.noreply.github.com>
2023-07-09 09:52:25 -0500

    Update MantaDebuffSilen.js

--- MantaDebuffSilen.js
+++ MantaDebuffSilen.js
@@ -153,7 +153,7 @@
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/AutoSaverWindrunner.ts"]();
+/******/ 	__webpack_modules__["./src/ItemsDogde.ts"]();
 /******/ 	
 /******/ })()
 ;
